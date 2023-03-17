e79fba4b709baaa64e5a70ae118cd9eb0b4a8183
Alexander-Morin <alexm.14@outlook.com>
2023-03-16 20:07:40 -0700

    parallel

--- R/00_config.R
+++ R/00_config.R
@@ -3,9 +3,11 @@
 
 library(future)
 
-# Prepare future parallel and increase memory max size for Seurat
+# Prepare future:: parallel and increase memory max size for Seurat
 plan("multisession", workers = 8)  
 options(future.globals.maxSize = 2097152000)
 
+# Cores for parallel::
+ncore <- 8
 
 plot_dir <- "/home/amorin/Plots/TR_singlecell/"
