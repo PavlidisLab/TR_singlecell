7c09124b177b6404a0adbfe675ee98f9a75ab28a
Alexander-Morin <alexm.14@outlook.com>
2023-03-16 17:07:35 -0700

    init util functions

---
+++ R/utils/functions.R
@@ -0,0 +1,74 @@
+## Project functions
+## -----------------------------------------------------------------------------
+
+
+library(parallel)
+
+
+
+top_expr_quantile <- function(sdat, gene, qtl = 0.9) {
+  
+  expr_cutoff <- quantile(sdat@assays$RNA@data[gene, ], qtl)
+  expr_cutoff <- ifelse(expr_cutoff == 0L, 1, expr_cutoff)
+  sdat$Top_expr_quantile <- sdat@assays$RNA@data[gene, ] >= expr_cutoff
+  
+  return(sdat)
+}
+
+
+
+top_expr_celltype <- function(sdat, avg_mat, gene) {
+  
+  stopifnot("Cell_type" %in% colnames(sdat@meta.data))
+  
+  top_ct <- names(which.max(avg_mat[gene , ]))
+  sdat$Top_expr_celltype <- sdat$Cell_type == top_ct
+  
+  return(sdat)
+}
+
+
+
+get_ct_avg <- function(sdat, assay = "RNA", scale = FALSE, ncores = 8) {
+  
+  stopifnot("Cell_type" %in% colnames(sdat@meta.data))
+  
+  cts <- unique(sdat$Cell_type)
+  
+  if (scale) {
+    mat <- sdat@assays[[assay]]@scale.data
+  } else {
+    mat <- sdat@assays[[assay]]@data
+  }
+  
+  ct_avg <- mclapply(cts, function(x) {
+    rowMeans(mat[, sdat$Cell_type == x])
+  }, mc.cores = ncores)
+  
+  ct_avg <- do.call(cbind, ct_avg)
+  rownames(ct_avg) <- rownames(mat)
+  colnames(ct_avg) <- cts
+  return(ct_avg)
+}
+
+
+
+mat_to_df <- function(mat, symmetric = TRUE) {
+  
+  if (symmetric) {
+    df <- data.frame(
+      Row = rownames(mat)[row(mat)[lower.tri(mat)]],
+      Col = colnames(mat)[col(mat)[lower.tri(mat)]],
+      Value = mat[lower.tri(mat)],
+      stringsAsFactors = FALSE
+    )
+  } else {
+    df <- data.frame(
+      Row = rownames(mat)[row(mat)],
+      Col = colnames(mat)[col(mat)],
+      Value = c(mat),
+      stringsAsFactors = FALSE
+    )
+  }
+  return(df)
+}
