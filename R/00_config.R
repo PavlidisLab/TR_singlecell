23b16709c46ffdf961f7e0359d75a6ef988ab887
Alexander-Morin <alexm.14@outlook.com>
2023-03-27 13:36:56 -0700

    hpa path

--- R/00_config.R
+++ R/00_config.R
@@ -4,10 +4,16 @@
 library(future)
 
 # Prepare future:: parallel and increase memory max size for Seurat
-plan("multisession", workers = 8)  
+plan("multisession", workers = 8)
 options(future.globals.maxSize = 2097152000)
 
 # Cores for parallel::
 ncore <- 8
 
 plot_dir <- "/home/amorin/Plots/TR_singlecell/"
+
+
+# Human Protein Atlas
+
+expr_mat_l_path <- "/home/amorin/scratch/R_objects/HPA_expression_mat_list.RDS"
+cor_mat_l_path <- "/home/amorin/scratch/R_objects/HPA_cor_mat_list.RDS"
