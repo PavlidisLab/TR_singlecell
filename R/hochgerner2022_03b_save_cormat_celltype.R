9538e384065059cc76f05e9a6c3a4219d6ce50e3
Alexander-Morin <alexm.14@outlook.com>
2023-03-27 18:15:35 -0700

    add a min non zero count threshold

--- R/hochgerner2022_03b_save_cormat_celltype.R
+++ R/hochgerner2022_03b_save_cormat_celltype.R
@@ -21,8 +21,8 @@ cts <- unique(sdat$Cell_type)
 # Subset of genes to look at
 tfs <- c("Ascl1", "Hes1", "Mecp2", "Mef2c", "Neurod1", "Pax6", "Runx1", "Tcf4")
 
-# Prepare count mat
-mat <- t(as.matrix(sdat@assays$RNA@data))
+# Min count of non-zero expressing cells to keep gene for correlating
+min_count <- 10
 
 
 # Loop over TFs and cor with every other gene
@@ -31,9 +31,16 @@ cor_ct <- lapply(cts, function(x) {
   
   message(paste(x, Sys.time()))
   
-  cells <- sdat$Cell_type == x
+  # Isolate cells from cell type. If not enough cells meet the minimum non-zero
+  # threshold, set all counts to NA (producing an NA cor)
   
-  cmat <- WGCNA::cor(x = mat[cells, ],
+  cells <- sdat$Cell_type == x
+  mat <- t(as.matrix(sdat@assays$RNA@data[, cells]))
+  na_genes <- apply(mat, 2, function(x) sum(x != 0)) <= min_count
+  mat[, na_genes] <- NA
+  
+  cmat <- WGCNA::cor(x = mat,
+                     y = mat[, tfs], 
                      nThreads = ncore,
                      use = "pairwise.complete.obs")
 })
