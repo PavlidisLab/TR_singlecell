02198885b2856fbe11957f710c1df2e92ba2bcf7
Alexander-Morin <alexm.14@outlook.com>
2023-05-01 18:29:23 -0700

    add paths

--- R/00_config.R
+++ R/00_config.R
@@ -13,6 +13,17 @@ ncore <- 8
 plot_dir <- "/home/amorin/Plots/TR_singlecell/"
 
 
+# Location of aggregate coexpression matrices
+amat_dir <- "~/scratch/R_objects/"
+
+
+# 1:1 orthologous protein coding genes
+pc_ortho_path <- "/space/grp/amorin/Metadata/hg_mm_1to1_ortho_genes_DIOPT-v8.tsv"
+
+# TR-target rankings from genomics evidence
+evidence_path <- "/space/scratch/amorin/R_objects/ranked_target_list_Apr2022.RDS"
+
+
 # Human Protein Atlas
 
 expr_mat_l_path <- "/home/amorin/scratch/R_objects/HPA_expression_mat_list.RDS"
