b557c973c3cbea3ace584aa327907976ba08a41e
Alexander-Morin <alexm.14@outlook.com>
2023-05-01 18:30:11 -0700

    init scratch script to compare cor between counts and normalized

---
+++ R/check_diff_cor_by_norm.R
@@ -0,0 +1,20 @@
+library(WGCNA)
+library(tidyverse)
+library(Matrix)
+source("R/utils/functions.R")
+source("R/utils/agg_functions.R")
+
+id <- "GSE216019"
+sc_dir <- paste0("/cosmos/data/downloaded-data/sc_datasets_w_supplementary_files/lab_projects_datasets/amorin_sc_datases/Human/", id)
+dat_path <- file.path(sc_dir, paste0(id, ".RDS"))
+out_path <- paste0("/space/scratch/amorin/R_objects/", id, "_mat_and_meta.RDS")
+pc <- read.delim("/home/amorin/Data/Metadata/ensembl_human_protein_coding_105.tsv", stringsAsFactors = FALSE)
+
+
+dat <- readRDS(dat_path)
+
+meta <- dat@meta.data %>% 
+  dplyr::rename(Cell_type = cell_type) %>% 
+  rownames_to_column(var = "ID")
+
+mat <- as.matrix(GetAssayData(dat, slot = "data"))
