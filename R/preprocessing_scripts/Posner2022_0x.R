a7f2e299a733430b9f943730910221fc43d780b6
Alexander-Morin <alexm.14@outlook.com>
2023-05-22 11:29:09 -0700

    updated workflow

--- R/preprocessing_scripts/Posner2022_0x.R
+++ R/preprocessing_scripts/Posner2022_0x.R
@@ -1,55 +1,74 @@
+## Process count matrix and get aggregate correlation for Posner2022
+## -----------------------------------------------------------------------------
+
 library(WGCNA)
 library(tidyverse)
-library(Matrix)
+library(Seurat)
+source("R/00_config.R")
 source("R/utils/functions.R")
-source("R/utils/agg_functions.R")
+source("R/utils/plot_functions.R")
 
-sc_dir <- "/cosmos/data/downloaded-data/sc_datasets_w_supplementary_files/lab_projects_datasets/amorin_sc_datases/Mouse/Posner2022/"
-dat_path <- file.path(sc_dir, "Posner2022.RDS")
-out_path <- "/space/scratch/amorin/R_objects/Posner2022_mat_and_meta.RDS"
-pc <- read.delim("/home/amorin/Data/Metadata/ensembl_mouse_protein_coding_105.tsv", stringsAsFactors = FALSE)
+id <- "Posner2022"
+sc_dir <- file.path("/cosmos/data/downloaded-data/sc_datasets_w_supplementary_files/lab_projects_datasets/amorin_sc_datases/Mouse/", id)
+dat_path <- file.path(sc_dir, paste0(id, ".RDS"))
+out_dir <- file.path("/space/scratch/amorin/TR_singlecell", id)
+processed_path <- file.path(out_dir, paste0(id, "_clean_mat_and_meta.RDS"))
+pc <- read.delim(ens_mm_path, stringsAsFactors = FALSE)
 
 
-if (!file.exists(out_path)) {
+if (!file.exists(processed_path)) {
   
   dat <- readRDS(dat_path)
   
+  # Extract count matrix
+  
+  mat <- as.matrix(GetAssayData(dat, slot = "counts"))
+  
+  # Ready metadata
+  
   meta <- dat@meta.data %>% 
     dplyr::rename(Cell_type = cell_type) %>% 
-    rownames_to_column(var = "ID")
+    rownames_to_column(var = "ID") %>% 
+    add_count_info(mat = mat)
   
-  mat <- as.matrix(GetAssayData(dat, slot = "data"))
+  # QC plots
   
-  stopifnot(identical(colnames(mat), meta$ID))
+  p1 <- all_hist(meta)
+  p2 <- qc_scatter(meta)
   
-  # Ensembl IDs to gene symbols
-  common_ens_ids <- intersect(pc$Gene_ID, rownames(mat))
+  ggsave(p1, device = "png", dpi = 300, height = 12, width = 16, bg = "white",
+         filename = file.path(out_dir, paste0(id, "_QC_histograms.png")))
   
-  common_genes <- data.frame(
-    Ensembl_ID = common_ens_ids,
-    Symbol = pc$Symbol[match(common_ens_ids, pc$Gene_ID)])
+  ggsave(p2, device = "png", dpi = 300, height = 8, width = 8,
+         filename = file.path(out_dir, paste0(id, "_QC_scatter.png")))
   
-  mat <- mat[common_genes$Ensembl_ID, meta$ID]
-  rownames(mat) <- common_genes$Symbol
-
-  zero_genes <- names(which(apply(mat, 1, function(x) sum(x != 0)) == 0))
-  keep_genes <- unique(setdiff(rownames(mat), zero_genes))
-  keep_genes <- keep_genes[keep_genes != ""]
-
-  mat <- mat[keep_genes, ]
+  # Remove cells failing QC, keep only protein coding genes, and normalize
   
-  saveRDS(list(mat, meta), file = out_path)
+  mat <- rm_low_qc_cells(mat, meta) %>%
+    ensembl_to_symbol(ensembl_df = pc) %>% 
+    get_pcoding_only(pcoding_df = pc) %>% 
+    Seurat::LogNormalize(., verbose = FALSE)
+  
+  meta <- filter(meta, ID %in% colnames(mat))
+  
+  stopifnot(all(colnames(mat) %in% meta$ID), length(meta$ID) > 0)
+  
+  saveRDS(list(mat, meta), file = processed_path)
   
   rm(dat)
   gc()
   
 } else {
   
-  dat <- readRDS(out_path)
+  dat <- readRDS(processed_path)
   meta <- dat[[2]]
   mat <- dat[[1]]
   
 }
+
+
+stopifnot(identical(colnames(mat), meta$ID))
+
 
 
 rsr1 <- all_RSR_aggregate1(mat, meta)
