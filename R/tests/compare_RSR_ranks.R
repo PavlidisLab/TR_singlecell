c740ff7393efccf601eb6ba0871abcfc04584a34
Alexander-Morin <alexm.14@outlook.com>
2023-05-25 12:03:59 -0700

    updated "final" matrices loaded and comparison df made

--- R/tests/compare_RSR_ranks.R
+++ R/tests/compare_RSR_ranks.R
@@ -8,22 +8,38 @@ source("R/utils/plot_functions.R")
 
 dat <- readRDS("/space/scratch/amorin/TR_singlecell/GSE180928/GSE180928_clean_mat_and_meta.RDS")
 rsr_all <- readRDS("/space/scratch/amorin/TR_singlecell/GSE180928/GSE180928_RSR_allrank.RDS")
+rsr_all <- lowertri_to_symm(rsr_all)
 rsr_col <- readRDS("/space/scratch/amorin/TR_singlecell/GSE180928/GSE180928_RSR_colrank.RDS")
 zcor <- readRDS("/space/scratch/amorin/TR_singlecell/GSE180928/GSE180928_fishersZ.RDS")
+na_mat <- zcor$NA_mat
+
+stopifnot(identical(rownames(rsr_all), rownames(rsr_col)))
+stopifnot(identical(rownames(rsr_all), rownames(zcor$Avg_all)))
+stopifnot(identical(rownames(rsr_all), rownames(zcor$NA_mat)))
 
 
-# TODO: replace with proper output
+gene <- "MECP2"
 
-saveRDS(
-  list(
-    Col_std = amat_colrank_std,
-    All_std = amat_allrank_std,
-    All_nostd = amat_allrank_nostd
-  ),
-  file = "/space/scratch/amorin/R_objects/Posner2022_subset_RSR_comparison.RDS"
+
+rank_df <- data.frame(
+  Symbol = rownames(rsr_all),
+  RSR_all = rsr_all[, gene],
+  RSR_col = rsr_col[, gene],
+  Zcor_all = zcor$Avg_all[, gene],
+  Zcor_nonNA = zcor$Avg_nonNA[, gene],
+  N_na = na_mat[, gene]
 )
 
-gene <- "Rpl37a"
+
+cor(select_if(rank_df, is.numeric))
+
+
+tt <- mat_to_df(rsr_all, symmetric = TRUE)
+
+
+tt2 <- rank_df %>% slice_max(RSR_all, n = 1000)
+cor(select_if(tt2, is.numeric))
+
 
 cor(amat_colrank_std[, gene], amat_allrank_std[, gene], method = "spearman", use = "pairwise.complete.obs")
 plot(amat_colrank_std[, gene], amat_allrank_std[, gene])
