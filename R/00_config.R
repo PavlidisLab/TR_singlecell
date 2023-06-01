684a161a136f569ab61ef3ca6fc13c50a2b5f1d2
Alexander-Morin <alexm.14@outlook.com>
2023-05-31 20:49:20 -0700

    update amat dir and include gsheets id

--- R/00_config.R
+++ R/00_config.R
@@ -11,7 +11,7 @@ plot_dir <- "/home/amorin/Plots/TR_singlecell/"
 
 
 # Location of aggregate coexpression matrices
-amat_dir <- "~/scratch/R_objects/"
+amat_dir <- "/space/scratch/amorin/TR_singlecell/"
 
 
 # 1:1 orthologous protein coding genes
@@ -26,6 +26,9 @@ ens_mm_path <- "/space/grp/amorin/Metadata/ensembl_mouse_protein_coding_105.tsv"
 
 # TR-target rankings from genomics evidence
 evidence_path <- "/space/scratch/amorin/R_objects/ranked_target_list_Apr2022.RDS"
+
+# Gsheets ID for TRsc datasets
+gsheets_id <- "1SQx_rFdBNBwOEdChaHkbQY1vStcbuA3fF0CZk6YECEc"
 
 
 # Human Protein Atlas
