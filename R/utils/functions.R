807a119b5317504245c002fa95dc7cf7763486bc
Alexander-Morin <alexm.14@outlook.com>
2023-06-03 14:03:46 -0700

    move unused max_cor_df; add gene_vec_to_mat()

--- R/utils/functions.R
+++ R/utils/functions.R
@@ -409,34 +409,14 @@ mat_to_df <- function(mat, symmetric = TRUE) {
 }
 
 
-# Return a df of the max cor for each cell type for the given gene, removing NAs
-# and the gene itself to prevent cor=1
 
-max_cor_df <- function(cmat_list, tf) {
-  
-  cor_max <- lapply(names(cmat_list), function(x) {
-    
-    cor_mat <- cmat_list[[x]]
-    
-    vec <- cor_mat[tf, setdiff(colnames(cor_mat), tf)]
-    
-    if (all(is.na(vec))) {
-      return(NA)
-    }
-    
-    data.frame(
-      Cell_type = x,
-      Value = max(vec, na.rm = TRUE),
-      Symbol = names(vec)[which.max(vec)])
-  })
-  
-  cor_max <- cor_max[!is.na(cor_max)]
-  
-  cor_max <- data.frame(do.call(rbind, cor_max)) %>% 
-    arrange(desc(Value))
-  
-  return(cor_max)
+
+# Bind all gene vectors of all matrices in agg_l into its own matrix
+
+gene_vec_to_mat <- function(agg_l, gene) {
+  do.call(cbind, lapply(agg_l, function(x) x[gene, ]))
 }
+
 
 
 
