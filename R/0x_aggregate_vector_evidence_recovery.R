d12844f1f5d618d36ed977cb6aaef842c9a8bb21
Alexander-Morin <alexm.14@outlook.com>
2023-06-05 17:36:40 -0700

    add analysis for aggregate performance; add every gene rank recovery of evidence

--- R/0x_aggregate_vector_evidence_recovery.R
+++ R/0x_aggregate_vector_evidence_recovery.R
@@ -1,4 +1,7 @@
-## TODO
+## TODO: point to missing genes
+## TODO: missing genes excluded from rank df makes a difference?
+## TODO: common process for curated and top k evidence for get_rank_df
+## TODO: remove tf?
 ## -----------------------------------------------------------------------------
 
 library(tidyverse)
@@ -42,102 +45,256 @@ genes_mm <- rownames(agg_mm[[1]])
 # ------------------------------------------------------------------------------
 
 
+# TODO: smarter
 
-which_ranked_auprc <- function(rank_vec, 
-                               rank_mat, 
-                               gene, 
-                               k = 1000,
-                               ncores = 1) {
+get_rank_df <- function(gene_vec, 
+                        evidence_df, 
+                        evidence_col,
+                        k = 1000) {
   
-  genes <- rownames(rank_mat)
-  x <- sort(rank_vec, decreasing = TRUE)
-  x <- x[names(x) != gene]
   
-  auprc_l <- mclapply(genes, function(g) {
+  rank_df <- if (evidence_col == "Curated_target") {
     
-    y <- sort(rank_mat[, g], decreasing = TRUE)[1:(k+1)]
-    y <- y[names(y) != g]
+    data.frame(Score = gene_vec) %>%
+      rownames_to_column(var = "Symbol") %>%
+      left_join(evidence_df[, c("Symbol", evidence_col)], by = "Symbol") %>%
+      # filter(Symbol != tf) %>%
+      arrange(!!sym(evidence_col)) %>%
+      mutate(Label = Symbol %in% filter(evidence_df, Curated_target)$Symbol) %>%
+      arrange(desc(Score))
     
-    rank_df <- data.frame(
-      Symbol = names(x),
-      Rank = x,
-      Label = names(x) %in% names(y)
-    )
+  } else {
     
-    if (all(rank_df$Label)) return(1)
-    if (all(!rank_df$Label)) return(0)
+    data.frame(Score = gene_vec) %>%
+      rownames_to_column(var = "Symbol") %>%
+      left_join(evidence_df[, c("Symbol", evidence_col)], by = "Symbol") %>%
+      # filter(Symbol != tf) %>%
+      arrange(!!sym(evidence_col)) %>%
+      mutate(Label = c(rep(1, k), rep(0, nrow(.) - k))) %>%
+      arrange(desc(Score))
     
-    get_au_perf(rank_df, label_col = "Label", score_col = "Rank", measure = "AUPRC")
-    
-  }, mc.cores = ncores)
-  names(auprc_l) <- genes
-  
-  rank_auprc <- sort(unlist(auprc_l), decreasing = TRUE)
-  rank_ix <- which(names(rank_auprc) == gene)
-  
-  return(list(Rank = rank_ix, List = rank_auprc))
-}
-
-
-
-rank_similarity_matrix <- function(agg_l, 
-                                   gene, 
-                                   msr,  # AUPRC|Intersect|Cor
-                                   k = 1000,
-                                   ncores = 1) {
-  
-  ids <- names(agg_l)
-  genes <- rownames(agg_l[[1]])
-  
-  stopifnot(length(ids) > 0, gene %in% genes)
-  
-  rank_mat <- matrix(1, nrow = length(agg_l), ncol = length(agg_l))
-  rownames(rank_mat) <- colnames(rank_mat) <- ids
-  
-  for (i in ids) {
-    for (j in ids) {
-      if (i == j) {
-        next
-      }
-      
-      rank_ix <- if (msr == "AUPRC") {
-        
-        which_ranked_auprc(
-          rank_vec = agg_l[[i]][, gene],
-          rank_mat = agg_l[[j]],
-          gene = gene,
-          k = k,
-          ncores = ncores)$Rank
-        
-      } else if (msr == "Intersect") {
-        
-        which_ranked_intersect(
-          rank_vec = agg_l[[i]][, gene],
-          rank_mat = agg_l[[j]],
-          gene = gene,
-          k = k,
-          ncores = ncores)$Rank
-        
-      } else {
-        
-        which_ranked_cor(
-          rank_vec = agg_l[[i]][, gene],
-          rank_mat = agg_l[[j]],
-          gene = gene,
-          ncores = ncores)$Rank
-      }
-      
-      rank_mat[i, j] <- rank_ix
-    }
   }
   
-  return(rank_mat)
+  return(rank_df)
+}
+
+
+# TODO:
+
+get_tf_performance <- function(agg_l,
+                               evidence_df,
+                               evidence_col,
+                               tf,
+                               k = 1000) {
+  
+  # TODO:
+  sc_tf_mat <- gene_vec_to_mat(agg_l, tf)
+  sc_tf_mat <- cbind(sc_tf_mat, Aggregate_rank = rowMeans(sc_tf_mat))
+  
+  # TODO:
+  auprc_l <- lapply(colnames(sc_tf_mat), function(x) {
+    gene_vec <- sc_tf_mat[, x]
+    rank_df <- get_rank_df(gene_vec, evidence, evidence_col, k)
+    get_au_perf(rank_df, label_col = "Label", score_col = "Score", measure = "AUPRC")
+  })
+  names(auprc_l) <- colnames(sc_tf_mat)
+  auprc_l <- unlist(auprc_l)
+  
+  return(auprc_l)
 }
 
 
 
-# 
+# TODO: 
+
+get_all_performance <- function(agg_l,
+                                evidence_df,
+                                evidence_col,
+                                genes,
+                                k = 1000,
+                                ncores = 1) {
+  
+  # TODO:
+  
+  perf_l <- lapply(agg_l, function(agg_mat) {
+    
+    # TODO:
+    
+    perf <- mclapply(genes, function(x) {
+      
+      gene_vec <- agg_mat[, x]
+      rank_df <- get_rank_df(gene_vec, evidence_df, evidence_col, k)
+      auprc <- get_au_perf(rank_df, label_col = "Label", score_col = "Score", measure = "AUPRC")
+      topk <- sum(rank_df$Label[1:k])
+      data.frame(AUPRC = auprc, Topk = topk)
+      
+    }, mc.cores = ncores)
+    
+    perf_df <- data.frame(Symbol = genes, do.call(rbind, perf)) %>%
+      arrange(desc(AUPRC))
+    
+    return(perf_df)
+    
+  })
+  
+  names(perf_l) <- names(agg_l)
+  return(perf_l)
+}
+
+
+
+# Individual versus aggregate recovery
 # ------------------------------------------------------------------------------
 
 
+# Human
 
+tf_performance_hg <- lapply(tfs_hg, function(tf) {
+  
+  get_tf_performance(agg_l = agg_hg, 
+                     evidence_df = evidence_l$Human[[tf]], 
+                     evidence_col = "Rank_binding", 
+                     tf = tf, 
+                     k = 1000)
+})
+names(tf_performance_hg) <- tfs_hg
+
+
+
+# Mouse
+
+tf_performance_mm <- lapply(tfs_mm, function(tf) {
+  
+  get_tf_performance(agg_l = agg_mm, 
+                     evidence_df = evidence_l$Mouse[[tf]], 
+                     evidence_col = "Rank_binding", 
+                     tf = tf, 
+                     k = 1000)
+})
+names(tf_performance_mm) <- tfs_mm
+
+
+
+
+
+# Every gene recovery of evidence (slow!)
+# ------------------------------------------------------------------------------
+
+
+outfile_hg1 <- "/space/scratch/amorin/R_objects/05-06-2023_coexpr_agg_recovery_integrated_evidence_human.RDS"
+outfile_hg2 <- "/space/scratch/amorin/R_objects/05-06-2023_coexpr_agg_recovery_curated_evidence_human.RDS"
+outfile_mm1 <- "/space/scratch/amorin/R_objects/05-06-2023_coexpr_agg_recovery_integrated_evidence_mouse.RDS"
+outfile_mm2 <- "/space/scratch/amorin/R_objects/05-06-2023_coexpr_agg_recovery_curated_evidence_mouse.RDS"
+
+
+# Human
+
+if (!file.exists(outfile_hg1)) {
+  
+  hg1 <- lapply(tfs_hg, function(tf) {
+    
+    get_all_performance(agg_l = agg_hg,
+                        evidence_df = evidence_l$Human[[tf]],
+                        evidence_col = "Rank_integrated",
+                        genes = genes_hg,
+                        k = 1000,
+                        ncores = ncore)
+  })
+  names(hg1) <- tfs_hg
+  
+  saveRDS(hg1, outfile_hg1)
+}
+
+
+
+if (!file.exists(outfile_hg2)) {
+  
+  hg2 <- lapply(tfs_hg, function(tf) {
+    
+    get_all_performance(agg_l = agg_hg,
+                        evidence_df = evidence_l$Human[[tf]],
+                        evidence_col = "Curated_target",
+                        genes = genes_hg,
+                        k = 1000,
+                        ncores = ncore)
+  })
+  names(hg2) <- tfs_hg
+  
+  saveRDS(hg2, outfile_hg2)
+}
+
+
+
+# Mouse
+
+
+if (!file.exists(outfile_mm1)) {
+  
+  mm1 <- lapply(tfs_mm, function(tf) {
+    
+    get_all_performance(agg_l = agg_mm,
+                        evidence_df = evidence_l$Mouse[[tf]],
+                        evidence_col = "Rank_integrated",
+                        genes = genes_mm,
+                        k = 1000,
+                        ncores = ncore)
+  })
+  names(mm1) <- tfs_mm
+  
+  saveRDS(mm1, outfile_mm1)
+}
+
+
+
+if (!file.exists(outfile_mm2)) {
+  
+  mm2 <- lapply(tfs_mm, function(tf) {
+    
+    get_all_performance(agg_l = agg_mm,
+                        evidence_df = evidence_l$Mouse[[tf]],
+                        evidence_col = "Curated_target",
+                        genes = genes_mm,
+                        k = 1000,
+                        ncores = ncore)
+  })
+  names(mm2) <- tfs_mm
+  
+  saveRDS(mm2, outfile_mm2)
+}
+
+
+
+# which_tf_auprc <- lapply(auprc_l3, function(x) which(x$Symbol == tf))
+# 
+# 
+# i <- 5
+# 
+# hist(auprc_l3[[i]]$Topk, breaks = 100)
+# abline(v = filter(auprc_l3[[i]], Symbol == tf)$Topk, col = "red")
+# 
+# hist(auprc_l3[[i]]$AUPRC, breaks = 100)
+# abline(v = filter(auprc_l3[[i]], Symbol == tf)$AUPRC, col = "red")
+# 
+# 
+# plot(auprc_l3$GSE180928$Topk, auprc_l3$GSE180928$AUPRC)
+# cor(auprc_l3$GSE180928$Topk, auprc_l3$GSE180928$AUPRC, method = "spearman")
+
+
+# Tally the genes whose aggregate expression vector was among the top k at 
+# recovering evidence (also top k)
+
+# tally_topk <- lapply(auprc_l3, function(x) x$Symbol[1:k]) %>% 
+#   unlist() %>% 
+#   table() %>% 
+#   sort(decreasing = TRUE)
+
+
+
+
+# Notice that while RUNX1 coexpression itself is not highly ranked, the genes
+# that are highly ranked (by their coexpression recovry of RUNX1 targets) are
+# also highly ranked RUNX1 targets...
+
+# filter(evidence, Symbol %in% names(tally_topk)[1:100]
+
+       
