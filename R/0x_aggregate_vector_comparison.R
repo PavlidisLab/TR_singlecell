deaf9cc531ef113a5bed77026f104049386de8be
Alexander-Morin <alexm.14@outlook.com>
2023-06-07 11:01:48 -0700

    add similarity_matrix(); inspection of single genes

--- R/0x_aggregate_vector_comparison.R
+++ R/0x_aggregate_vector_comparison.R
@@ -3,7 +3,9 @@
 ## network. The main idea idea is to find the rank position of the gene of
 ## interest with itself in another network.
 ## TODO: function argument of main call
+## TODO: collapse main function (general structure of nested loop to mat)
 ## TODO: lapply call of main either made into a function or reduced to relevant only
+## TODO: process/save/load function
 ## -----------------------------------------------------------------------------
 
 library(tidyverse)
@@ -199,7 +201,142 @@ rank_similarity_matrix <- function(agg_l,
 
 
 
-# Calling over numerous genes - very slow
+
+similarity_matrix <- function(agg_l, 
+                              gene, 
+                              msr,  # AUPRC|Intersect
+                              k = 1000,
+                              ncores = 1) {
+  
+  ids <- names(agg_l)
+  genes <- rownames(agg_l[[1]])
+  
+  stopifnot(length(ids) > 0, gene %in% genes)
+  
+  sim_mat <- matrix(1, nrow = length(agg_l), ncol = length(agg_l))
+  rownames(sim_mat) <- colnames(sim_mat) <- ids
+  
+  for (i in ids) {
+    for (j in ids) {
+      if (i == j) {
+        next
+      }
+      
+      x <- sort(agg_l[[i]][, gene], decreasing = TRUE)
+      x <- x[names(x) != gene]
+      
+      y <- sort(agg_l[[j]][, gene], decreasing = TRUE)[1:(k+1)]
+      y <- y[names(y) != i]
+      
+      sim <- if (msr == "AUPRC") {
+        
+        rank_df <- data.frame(
+          Symbol = names(x),
+          Rank = x,
+          Label = names(x) %in% names(y)
+        )
+        
+        if (all(rank_df$Label)) return(1)
+        if (all(!rank_df$Label)) return(0)
+        
+        get_au_perf(rank_df, label_col = "Label", score_col = "Rank", measure = "AUPRC")
+        
+      } else if (msr == "Intersect") {
+       
+        x <- x[1:k]
+        length(intersect(names(x), names(y)))
+        
+      }
+        
+      sim_mat[i, j] <- sim
+      
+    }
+  }
+  
+  return(sim_mat)
+}
+
+
+
+
+
+
+# Inspecting single genes
+# ------------------------------------------------------------------------------
+
+gene_hg <- "RPL19"  # RPL3
+gene_mm <- "Rpl19"
+
+
+# Cor
+
+gene_mat_hg <- gene_vec_to_mat(agg_hg, gene_hg)
+gene_cor_hg <- cor(gene_mat_hg, method = "spearman", use = "pairwise.complete.obs")
+cor_heatmap(gene_cor_hg)
+
+gene_mat_mm <- gene_vec_to_mat(agg_mm, gene_mm)
+gene_cor_mm <- cor(gene_mat_mm, method = "spearman", use = "pairwise.complete.obs")
+cor_heatmap(gene_cor_mm)
+
+
+# AUPRC
+
+gene_auprc_hg <- similarity_matrix(agg_l = agg_hg, gene = gene_hg, k = 1000, msr = "AUPRC")
+gene_auprc_mm <- similarity_matrix(agg_l = agg_mm, gene = gene_mm, k = 1000, msr = "AUPRC")
+
+pheatmap(gene_auprc_hg,
+         cluster_rows = FALSE,
+         cluster_cols = FALSE,
+         border_col = "black",
+         color = c('#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b'),
+         display_numbers = TRUE,
+         number_color = "black",
+         fontsize = 20,
+         cellwidth = 50,
+         cellheight = 50)
+
+
+# Topk
+
+gene_topk_hg <- similarity_matrix(agg_l = agg_hg, gene = gene_hg, k = 1000, msr = "Intersect")
+gene_topk_mm <- similarity_matrix(agg_l = agg_mm, gene = gene_mm, k = 1000, msr = "Intersect")
+
+
+
+
+# Comparing metrics
+
+
+comp_df <- data.frame(
+  Cor = c(
+    mat_to_df(gene_cor_hg, symmetric = TRUE)$Value,
+    mat_to_df(gene_cor_mm, symmetric = TRUE)$Value
+  ),
+  Topk = c(
+    mat_to_df(gene_topk_hg, symmetric = TRUE)$Value,
+    mat_to_df(gene_topk_mm, symmetric = TRUE)$Value
+  ),
+  AUPRC = c(
+    mat_to_df(gene_auprc_hg, symmetric = TRUE)$Value,
+    mat_to_df(gene_auprc_mm, symmetric = TRUE)$Value
+  )
+)
+
+
+
+
+ggplot(comp_df, aes(x = Cor, y = AUPRC)) +
+  geom_point(shape = 19, size = 3) +
+  ggtitle(gene_hg) +
+  theme_classic() +
+  theme(axis.text = element_text(size = 20),
+        axis.title = element_text(size = 20),
+        plot.title = element_text(size = 20))
+
+cor(comp_df)
+
+
+# Calling which rank over numerous genes (ribosomal + TF) - prohibitively slow
 # ------------------------------------------------------------------------------
 
 
@@ -214,31 +351,6 @@ outfile_cor_hg <- "/space/scratch/amorin/R_objects/03-06-2023_aggregate_vector_c
 outfile_int_mm <- "/space/scratch/amorin/R_objects/03-06-2023_aggregate_vector_comparison_intersect_mouse.RDS"
 outfile_auprc_mm <- "/space/scratch/amorin/R_objects/03-06-2023_aggregate_vector_comparison_auprc_mouse.RDS"
 outfile_cor_mm <- "/space/scratch/amorin/R_objects/03-06-2023_aggregate_vector_comparison_cor_mouse.RDS"
-
-# Correlation of gene's rankings across studies
-# gene_mat <- gene_vec_to_mat(agg_hg, gene)
-# gene_rank_cor <- cor(gene_mat, method = "spearman", use = "pairwise.complete.obs")
-
-
-# rmat_int_hg <- rank_similarity_matrix(agg_l = agg_hg, 
-#                                       gene = gene, 
-#                                       msr = "Intersect",
-#                                       k = 1000,
-#                                       ncores = ncore)
-# 
-# 
-# rmat_auprc_hg <- rank_similarity_matrix(agg_l = agg_hg,
-#                                         gene = gene,
-#                                         msr = "AUPRC",
-#                                         k = 1000,
-#                                         ncores = ncore)
-# 
-# 
-# rmat_cor_hg <- rank_similarity_matrix(agg_l = agg_hg, 
-#                                       gene = gene, 
-#                                       msr = "Cor",
-#                                       ncores = ncore)
-
 
 
 # Intersects
@@ -256,7 +368,10 @@ if (!file.exists(outfile_int_hg)) {
   })
   names(int_l_hg) <- test_genes_hg
   saveRDS(int_l_hg, outfile_int_hg)
+} else {
+  int_l_hg <- readRDS(outfile_int_hg)
 }
+
 
 
 
@@ -272,7 +387,10 @@ if (!file.exists(outfile_int_mm)) {
   })
   names(int_l_mm) <- test_genes_mm
   saveRDS(int_l_mm, outfile_int_mm)
+} else {
+  int_l_mm <- readRDS(outfile_int_mm)
 }
+
 
 
 # AUPRCs
@@ -290,8 +408,7 @@ if (!file.exists(outfile_auprc_hg)) {
   })
   names(auprc_l_hg) <- test_genes_hg
   saveRDS(auprc_l_hg, outfile_auprc_hg)
-}
-
+} 
 
 
 if (!file.exists(outfile_auprc_mm)) {
@@ -340,3 +457,68 @@ if (!file.exists(outfile_cor_mm)) {
   names(cor_l_mm) <- test_genes_mm
   saveRDS(cor_l_mm, outfile_cor_mm)
 }
+
+
+##
+
+
+int_l_hg[[gene_hg]]
+
+
+
+tt1 <- which_ranked_intersect(rank_vec = agg_hg[["GSE216019"]][, gene_hg], 
+                              rank_mat = agg_hg[["GSE180928"]],
+                              gene = gene_hg, 
+                              k = 1000, 
+                              ncores = ncore)
+tt1$Rank
+
+data.frame(Topk = tt1$List) %>% 
+  ggplot(aes(x = Topk)) +
+  geom_histogram(bins = 100) +
+  geom_vline(xintercept = tt1$List[gene_hg], col = "red") +
+  ylab("Count") +
+  xlab("Topk intersect") +
+  ggtitle("GSE216019 RPL3 in GSE180928") +
+  theme_classic() +
+  theme(axis.text = element_text(size = 20),
+        axis.title = element_text(size = 20),
+        plot.title = element_text(size = 20))
+
+
+
+# Collapse the ribosomal gene rankings: First standardize each rank (0 worst, 
+# 1 best), and then take the median of [exp1, exp2] ranks across ribo genes.
+
+med_standard_rank <- function(mat_l, max_rank) {
+  
+  # because current ranks are 1=best to max_rank=worst, reverse for standard
+  mat_l <- lapply(mat_l, function(x) (max_rank + 1 - x) / max_rank)
+  med_mat <- apply(simplify2array(mat_l), 1:2, median)
+  return(med_mat)
+}
+
+
+med_ribo_srank_hg <- med_standard_rank(int_l_hg[ribo_genes$Symbol_hg], max_rank = length(genes_hg))
+med_ribo_srank_mm <- med_standard_rank(int_l_mm[ribo_genes$Symbol_mm], max_rank = length(genes_mm))
+
+
+med_ribo_srank_df <- rbind(mat_to_df(diag_to_na(med_ribo_srank_hg)),
+                           mat_to_df(diag_to_na(med_ribo_srank_mm)))
+
+
+hist(med_ribo_srank_df$Value, breaks = 100)
+
+
+# Inspecting example of RPL19 being the top hit between datasets
+
+
+gene_topk_rank_hg <- which_ranked_intersect(
+  rank_vec = agg_hg$GSE202352[, "RPL19"],
+  rank_mat = agg_hg$GSE180928,
+  gene = "RPL19",
+  k = 1000,
+  ncores = ncore)
+
+
+gene_topk_rank_hg$List %>% head
