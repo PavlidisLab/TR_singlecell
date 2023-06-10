70486d4d305f58811e4f274e21a172e86fe45e20
Alexander-Morin <alexm.14@outlook.com>
2023-06-10 13:52:36 -0700

    doc

--- R/utils/preprocess_cellxgene.R
+++ R/utils/preprocess_cellxgene.R
@@ -1,4 +1,6 @@
-## 
+## This script takes in arguments for an experiment ID and species, loading the
+## associated ID's seurat object and pre-processing the data before generating
+## the aggregate correlation matrix.
 ## -----------------------------------------------------------------------------
 
 source("R/00_config.R")
@@ -7,16 +9,13 @@ source("R/utils/plot_functions.R")
 
 args <- commandArgs(trailingOnly = TRUE)
 id <- args[1]
-species <- args[2]
-
+species <- args[2]  
 
 sc_dir <- file.path("/cosmos/data/downloaded-data/sc_datasets_w_supplementary_files/lab_projects_datasets/amorin_sc_datasets", id)
 dat_path <- file.path(sc_dir, paste0(id, "_cellxgene_seurat.RDS"))
 out_dir <- file.path("/space/scratch/amorin/TR_singlecell", id)
 processed_path <- file.path(out_dir, paste0(id, "_clean_mat_and_meta.RDS"))
 allrank_path <- file.path(out_dir, paste0(id, "_RSR_allrank.RDS"))
-colrank_path <- file.path(out_dir, paste0(id, "_RSR_colrank.RDS"))
-zcor_path <- file.path(out_dir, paste0(id, "_fishersZ.RDS"))
 
 
 pc <- if (str_to_lower(species) %in% c("human", "hg")) {
@@ -93,15 +92,3 @@ if (!file.exists(allrank_path)) {
   rsr_all <- RSR_allrank(mat, meta)
   saveRDS(rsr_all, allrank_path)
 }
-
-
-# if (!file.exists(colrank_path)) {p1
-#   rsr_col <- RSR_colrank(mat, meta)
-#   saveRDS(rsr_col, colrank_path)
-# }
-# 
-# 
-# if (!file.exists(zcor_path)) {
-#   zcor <- fishersZ_aggregate(mat, meta)
-#   saveRDS(zcor, zcor_path)
-# }
