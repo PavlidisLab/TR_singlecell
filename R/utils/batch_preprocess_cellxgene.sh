198c7f60a85e3126471903c0948a737cb8be3388
Alexander-Morin <alexm.14@outlook.com>
2023-06-10 13:51:54 -0700

    init script to call preprocess_cellxgene.R over input tsv

---
+++ R/utils/batch_preprocess_cellxgene.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Input file assumes that the first column is the ID and the second column is
+# the species. Iterates over the fields of the input file, pre-processing the
+# associated data and generating an aggregate coexpression network.
+
+
+sc_dir="/cosmos/data/downloaded-data/sc_datasets_w_supplementary_files/lab_projects_datasets/amorin_sc_datasets/"
+input_file="${sc_dir}input_TRsc_download.tsv"
+preprocess="/home/amorin/Projects/TR_singlecell/R/utils/preprocess_cellxgene.R"
+
+
+while IFS=$'\t' read -r id species url; do
+
+  echo "Beginning $id"
+
+  Rscript --vanilla "$preprocess" "$id" "$species"
+
+done < "$input_file"
+
+
+
+
