9ecea84e5d91ac03f9f8893ece5b52713f14e26f
Alexander-Morin <alexm.14@outlook.com>
2023-06-12 17:02:50 -0700

    function and load/save logic for generating cor list

--- R/tests/celltype_cor_as_list.R
+++ R/tests/celltype_cor_as_list.R
@@ -1,34 +1,67 @@
 ## Generate cell type correlation into a list for inspection of intermediates of
 ## aggregation.
+## -----------------------------------------------------------------------------
+
+source("R/00_config.R")
+source("R/utils/functions.R")
+
+id <- "GSE180928"
+species <- "Human" 
+
+sc_dir <- file.path("/cosmos/data/downloaded-data/sc_datasets_w_supplementary_files/lab_projects_datasets/amorin_sc_datasets", id)
+dat_path <- file.path(sc_dir, paste0(id, "_cellxgene_seurat.RDS"))
+out_dir <- file.path("/space/scratch/amorin/TR_singlecell", id)
+processed_path <- file.path(out_dir, paste0(id, "_clean_mat_and_meta.RDS"))
+allrank_path <- file.path(out_dir, paste0(id, "_RSR_allrank.RDS"))
+corlist_path <- file.path(out_dir, paste0(id, "_celltype_corlist.RDS"))
 
 
+dat <- readRDS(processed_path)
+meta <- dat[[2]]
+mat <- as.matrix(dat[[1]])
+rsr <- readRDS(allrank_path)
+
+genes <- rownames(mat)
 
 
-# Return a df of the max cor for each cell type for the given gene, removing NAs
-# and the gene itself to prevent cor=1
-
-max_cor_df <- function(cmat_list, tf) {
+get_cor_list <- function(mat, meta, ncores = 1) {
   
-  cor_max <- lapply(names(cmat_list), function(x) {
-    
-    cor_mat <- cmat_list[[x]]
-    
-    vec <- cor_mat[tf, setdiff(colnames(cor_mat), tf)]
-    
-    if (all(is.na(vec))) {
-      return(NA)
-    }
-    
-    data.frame(
-      Cell_type = x,
-      Value = max(vec, na.rm = TRUE),
-      Symbol = names(vec)[which.max(vec)])
-  })
+  cts <- unique(meta$Cell_type)
   
-  cor_max <- cor_max[!is.na(cor_max)]
+  # Split mat into list of cell type matrices, setting low count genes to NA,
+  # calculating gene-cor cor for each cell type, and setting NA cors to 0
   
-  cor_max <- data.frame(do.call(rbind, cor_max)) %>% 
-    arrange(desc(Value))
+  ct_l <- mclapply(cts, function(x) {
+    
+    ct_mat <- subset_and_filter(mat = mat, 
+                                meta = meta, 
+                                cell_type = x, 
+                                min_count = 20)
+    
+    cor_mat <- ct_mat %>%
+      get_cor_mat(lower_tri = FALSE) %>%
+      na_to_zero() %>%
+      diag_to_one()
+    
+    return(cor_mat)
+    
+  }, mc.cores = ncores)
   
-  return(cor_max)
+  names(ct_l) <- cts
+  
+  return(ct_l)
 }
+
+
+
+if (!file.exists(corlist_path)) {
+  cor_l <- get_cor_list(mat = mat, meta = meta, ncores = ncore)
+  saveRDS(cor_l, corlist_path)
+} else {
+  cor_l <- readRDS(corlist_path)
+}
+
+  
+# Get the average cor across cell types
+
+# cor_avg <- apply(simplify2array(cor_l), 1:2, mean)
