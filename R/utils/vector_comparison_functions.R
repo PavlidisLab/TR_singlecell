c0619e991194db31523a9bb17d8df1edbfa2bc16
Alexander-Morin <alexm.14@outlook.com>
2023-06-26 13:14:14 -0700

    add topk_sort(); topk_intersect(); vec_auprc(); binarize_topk_btmk; add colwise similarity functions; update/refactor gene_rank_topk() -> query_gene_rank_topk()

--- R/utils/vector_comparison_functions.R
+++ R/utils/vector_comparison_functions.R
@@ -7,6 +7,141 @@
 source("R/utils/functions.R")
 
 
+# TODO:
+
+topk_sort <- function(vec, k) names(sort(vec, decreasing = TRUE)[1:k])
+
+
+# TODO:
+
+topk_intersect <- function(vec1, vec2) length(intersect(vec1, vec2))
+
+
+
+# TODO:
+
+vec_auprc <- function(vec1, vec2) {
+  
+  rank_df <- data.frame(Score = vec1, Label = names(vec1) %in% vec2)
+  if (all(rank_df$Label)) return(1)
+  if (all(!rank_df$Label)) return(0)
+  get_au_perf(rank_df, label_col = "Label", score_col = "Score", measure = "AUPRC")
+}
+
+
+  
+
+# Binarize matrix such that top k and bottom k is 1, everything else 0
+
+binarize_topk_btmk <- function(mat, k = 1000) {
+  
+  bin_mat <- apply(mat, 2, function(x) {
+    sort_values <- sort(x, decreasing = TRUE)
+    topk <- sort_values[k]
+    btmk <- sort_values[length(x) - k + 1]
+    ifelse(x >= topk | x <= btmk, 1, 0)
+  })
+  
+  return(bin_mat)
+}
+
+
+
+
+# TODO:
+
+colwise_topk_intersect <- function(mat, k = 1000) {
+  
+  col_list <- asplit(mat, 2)
+  topk_list <- lapply(col_list, topk_sort, k)
+  topk_mat <- outer(topk_list, topk_list, Vectorize(topk_intersect))
+  return(topk_mat)
+}
+
+
+# TODO:
+
+colwise_cor <- function(mat, cor_method = "spearman", ncores = 1) {
+  cor_mat <- WGCNA::cor(x = mat, method = cor_method, nThreads = ncores)
+  return(cor_mat)
+}
+
+
+
+# TODO:
+
+colwise_jaccard <- function(mat) {
+  
+  jaccard <- function(vec1, vec2) sum(vec1 & vec2) / sum(vec1 | vec2)  
+  
+  col_list <- asplit(mat, 2)
+  jacc_mat <- outer(col_list, col_list, Vectorize(jaccard))
+  return(jacc_mat)
+}
+
+
+
+# Nested loop faster than outer(): skipping diagonal outweighs vectorized
+
+colwise_topk_auprc <- function(mat, k = 1000) {
+  
+  auprc_mat <- matrix(1, nrow = ncol(mat), ncol = ncol(mat))
+  colnames(auprc_mat) <- rownames(auprc_mat) <- colnames(mat)
+  
+  for (i in 1:nrow(auprc_mat)) {
+    for (j in 1:ncol(auprc_mat)) {
+      if (i == j) next
+      scores <- sort(mat[, i], decreasing = TRUE)
+      labels <- topk_sort(mat[, j], k)
+      auprc_mat[i, j] <- vec_auprc(scores, labels)
+    }
+  }
+  
+  return(auprc_mat)
+}
+
+
+
+# rank_vec is a gene ranking vector (1=best)
+# rank_mat is a gene x gene (1=best)
+# gene is a pcoding gene of interest corresponding to rank_vec
+# k is cutoff for the top genes to use as labels for every column in rank_mat
+# ncores is number of cores
+# 
+# Calculate the size of the intersect between the topk genes in rank_vec and
+# the top k genes of every column of rank_mat (topk). Return a list of these 
+# sorted topks, as well as the rank (1=best) of gene's topk with its name-matched 
+# column in rank_mat
+
+query_gene_rank_topk <- function(query_vec,
+                                 subject_mat,
+                                 gene,
+                                 k = 1000,
+                                 ncores = 1) {
+  
+  
+  genes <- rownames(subject_mat)
+  
+  stopifnot(gene %in% genes, identical(names(query_vec), genes))
+  
+  query_topk <- topk_sort(query_vec, k)
+  
+  topk_l <- mclapply(genes, function(x) {
+    subject_topk <- topk_sort(subject_mat[, x], k)
+    topk_intersect(query_topk, subject_topk)
+  }, mc.cores = ncores)
+  
+  names(topk_l) <- genes
+  topk_sort <- sort(unlist(topk_l), decreasing = TRUE)
+  rank_ix <- which(names(topk_sort) == gene)
+
+  return(list(Rank = rank_ix, Topk = topk_sort))
+  # return(rank_ix)
+}
+
+
+
+
 # rank_vec is a gene ranking vector (1=best)
 # rank_mat is a gene x gene (1=best)
 # gene is a pcoding gene of interest corresponding to rank_vec
@@ -88,43 +223,6 @@ gene_rank_auprc <- function(rank_vec,
 
 
 
-# rank_vec is a gene ranking vector (1=best)
-# rank_mat is a gene x gene (1=best)
-# gene is a pcoding gene of interest corresponding to rank_vec
-# k is cutoff for the top genes to use as labels for every column in rank_mat
-# ncores is number of cores
-# 
-# Calculate the size of the intersect between the topk genes in rank_vec and
-# the top k genes of every column of rank_mat (topk). Return a list of these 
-# sorted topks, as well as the rank (1=best) of gene's topk with its name-matched 
-# column in rank_mat
-
-gene_rank_topk <- function(rank_vec,
-                           rank_mat,
-                           gene,
-                           k = 1000,
-                           ncores = 1) {
-  
-  genes <- rownames(rank_mat)
-  
-  stopifnot(gene %in% genes)
-  
-  rank_vec <- sort(rank_vec, decreasing = TRUE)
-  
-  topk_l <- mclapply(genes, function(x) {
-    
-    rank_vec2 <- sort(rank_mat[, x], decreasing = TRUE)[1:k]
-    
-    length(intersect(names(rank_vec), names(rank_vec2)))
-    
-  }, mc.cores = ncores)
-  names(topk_l) <- genes
-  
-  rank_topk <- sort(unlist(topk_l), decreasing = TRUE)
-  rank_ix <- which(names(rank_topk) == gene)
-  
-  return(list(Rank = rank_ix, List = rank_topk))
-}
 
 
 
@@ -394,3 +492,4 @@ which_rank_mat <- function(perf_l) {
   
   return(which_mat)
 }
+
