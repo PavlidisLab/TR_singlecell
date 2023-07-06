c51bde40d64eca1ecd7ef7595576c70221593e82
Alexander-Morin <alexm.14@outlook.com>
2023-07-05 18:14:50 -0700

    rm unused; add inspection of best ranked pairs

--- R/0x_aggregate_vector_comparison.R
+++ R/0x_aggregate_vector_comparison.R
@@ -41,8 +41,8 @@ ribo_genes <- filter(pc_ortho, Symbol_hg %in% c(sribo_hg$Approved.symbol, lribo_
 tfs <- c("Ascl1", "Hes1", "Mecp2", "Mef2c", "Neurod1", "Pax6", "Runx1", "Tcf4")
 
 # Genes to focus/subset on when loading aggregate coexpression matrices
-subset_hg <- c(str_to_upper(tfs), "RPL3", "EGR1")   # NULL
-subset_mm <- c(str_to_title(tfs), "Rpl3", "Egr1")   # NULL
+subset_hg <- c(str_to_upper(tfs), "RPL3", "EGR1", "FOS", "FOSB")   # NULL
+subset_mm <- c(str_to_title(tfs), "Rpl3", "Egr1", "Fos", "Fosb")   # NULL
 
 # Load aggregate matrix into list
 agg_hg <- load_agg_mat_list(ids = ids_hg, sub_genes = subset_hg, genes = pc_hg$Symbol)
@@ -72,10 +72,6 @@ gene_mat_mm <- gene_vec_to_mat(agg_mm, gene_mm)[, which(msr_mm[gene_mm, ] == 1)]
 gene_cor_hg <- colwise_cor(gene_mat_hg)
 gene_cor_mm <- colwise_cor(gene_mat_mm)
 
-# AUPRC
-gene_auprc_hg <- colwise_topk_auprc(gene_mat_hg)
-gene_auprc_mm <- colwise_topk_auprc(gene_mat_mm)
-
 # Topk
 gene_topk_hg <- colwise_topk_intersect(gene_mat_hg)
 gene_topk_mm <- colwise_topk_intersect(gene_mat_mm)
@@ -84,6 +80,9 @@ gene_topk_mm <- colwise_topk_intersect(gene_mat_mm)
 gene_jacc_hg <- binarize_topk_btmk(gene_mat_hg) %>% colwise_jaccard()
 gene_jacc_mm <- binarize_topk_btmk(gene_mat_mm) %>% colwise_jaccard()
 
+# AUPRC - skipped: slower and not symmetric. Highly cor with topk
+# gene_auprc_hg <- colwise_topk_auprc(gene_mat_hg)
+# gene_auprc_mm <- colwise_topk_auprc(gene_mat_mm)
 
 
 # Collect similarity metrics into a dataframe
@@ -99,10 +98,6 @@ gene_sim_df <-
       mat_to_df(gene_topk_hg, symmetric = TRUE)$Value,
       mat_to_df(gene_topk_mm, symmetric = TRUE)$Value
     ),
-    AUPRC = c(
-      mat_to_df(gene_auprc_hg, symmetric = TRUE)$Value,
-      mat_to_df(gene_auprc_mm, symmetric = TRUE)$Value
-    ),
     Jaccard = c(
       mat_to_df(gene_jacc_hg, symmetric = TRUE)$Value,
       mat_to_df(gene_jacc_mm, symmetric = TRUE)$Value
@@ -111,8 +106,7 @@ gene_sim_df <-
 
 
 
-ggplot(gene_sim_df, aes(x = AUPRC, y = Topk)) +
-# ggplot(gene_sim_df, aes(x = Scor, y = Topk)) +
+ggplot(gene_sim_df, aes(x = Scor, y = Topk)) +
   geom_point(shape = 19, size = 3) +
   ggtitle(gene_hg) +
   theme_classic() +
@@ -127,10 +121,18 @@ cor(select_if(gene_sim_df, is.numeric))
 
 
 
-rank_mat <- readRDS("~/scratch/R_objects/27-06-2023/ASCL1.RDS")
+# Loading the matrix showing a gene's similarity rank (by topk) with itself
+# in all pairs of datasets
 
+# rank_mat <- readRDS(paste0("~/scratch/R_objects/27-06-2023/", gene_hg, ".RDS"))
+rank_mat <- readRDS(paste0("~/scratch/R_objects/04-07-2023/", gene_hg, ".RDS"))
 
-rank_mat <- rank_mat[setdiff(rownames(rank_mat), na_hg), setdiff(colnames(rank_mat), na_hg)]
+# TODO: remove when ranks regenerated
+keep_exps <- intersect(rownames(rank_mat), names(which(msr_hg[gene_hg, ] == 1)))
+rank_mat <- rank_mat[keep_exps, keep_exps]
+gene_topk_hg <- gene_topk_hg[keep_exps, keep_exps]
+gene_sim_df <- filter(gene_sim_df, Row %in% keep_exps & Col %in% keep_exps)
+
 diag(rank_mat) <- NA
 
 
@@ -147,83 +149,106 @@ pheatmap(rank_mat,
          cellwidth = 50,
          cellheight = 50,
          height = 18,
-         width = 18,
-         filename = "tt.png")
+         width = 18)
 
 
 
-F1 <- query_gene_rank_topk(query_vec = agg_hg$NowickiOsuch2023[, gene_hg],
-                           subject_mat = load_agg_mat_list("Ravindra2021")[[1]],
-                           gene = gene_hg,
-                           ncores = ncore)
+# Pile up at the left of the histogram (lower/better ranks) suggests a signal,
+# as random ranking would be uniform
+
+hist(rank_mat, breaks = 20)
+hist(replicate(length(rank_mat), sample(1:length(pc_hg$Symbol), 1)), breaks = 20)
 
 
-data.frame(Topk = F1$Topk) %>% 
+
+# Inspecting top pairs: by topk magnitude, and by rank. The highest value/most
+# similar pair across all experiment pairs may not actually have the highest
+# gene similarity rank across all genes within the subject dataset. If there
+# are multiple pairs with the best rank, take the highest topk magnitude pair.
+
+best_value_id <- slice_max(gene_sim_df, Topk, n = 1)
+
+best_rank_ix <- which(rank_mat == min(rank_mat, na.rm = TRUE), arr.ind = TRUE)
+best_rank_ix <- best_rank_ix[which.max(gene_topk_hg[best_rank_ix, drop = FALSE]), ]
+best_rank_id <- data.frame(Row = rownames(rank_mat)[best_rank_ix[1]], Col = colnames(rank_mat)[best_rank_ix[2]])
+
+
+best_value <- query_gene_rank_topk(
+  query_vec = agg_hg[[best_value_id$Row]][, gene_hg],
+  subject_mat = load_agg_mat_list(best_value_id$Col, genes = pc_hg$Symbol)[[1]],
+  gene = gene_hg,
+  ncores = ncore)
+
+
+best_rank <- query_gene_rank_topk(
+  query_vec = agg_hg[[best_rank_id$Row]][, gene_hg],
+  subject_mat = load_agg_mat_list(best_rank_id$Col, genes = pc_hg$Symbol)[[1]],
+  gene = gene_hg,
+  ncores = ncore)
+
+
+
+pxa <- data.frame(Topk = best_value$Topk) %>% 
   ggplot(aes(x = Topk)) +
   geom_histogram(bins = 100) +
-  geom_vline(xintercept = F1$Topk[gene_hg], col = "red") +
+  geom_vline(xintercept = best_value$Topk[gene_hg], col = "red") +
   ylab("Count of genes") +
   xlab("Topk intersect") +
-  ggtitle("GSE216019 ASCL1 in GSE180928") +
   theme_classic() +
   theme(axis.text = element_text(size = 20),
         axis.title = element_text(size = 20),
         plot.title = element_text(size = 20))
 
 
-head(sort(table(F1$Topk), decreasing = TRUE))
+pxb <- data.frame(Topk = best_rank$Topk) %>% 
+  ggplot(aes(x = Topk)) +
+  geom_histogram(bins = 100) +
+  geom_vline(xintercept = best_rank$Topk[gene_hg], col = "red") +
+  ylab("Count of genes") +
+  xlab("Topk intersect") +
+  theme_classic() +
+  theme(axis.text = element_text(size = 20),
+        axis.title = element_text(size = 20),
+        plot.title = element_text(size = 20))
 
 
 
-# Collapse the ribosomal gene rankings: First standardize each rank (0 worst, 
-# 1 best), and then take the median of [exp1, exp2] ranks across ribo genes.
-
-med_standard_rank <- function(mat_l, max_rank) {
-  
-  # because current ranks are 1=best to max_rank=worst, reverse for standard
-  mat_l <- lapply(mat_l, function(x) (max_rank + 1 - x) / max_rank)
-  med_mat <- apply(simplify2array(mat_l), 1:2, median)
-  return(med_mat)
-}
+# Generate a null similarity matrix, pulling random genes from datasets with
+# matched expression with the gene of interest
 
 
-med_ribo_srank_hg <- med_standard_rank(int_l_hg[ribo_genes$Symbol_hg], max_rank = length(genes_hg))
-med_ribo_srank_mm <- med_standard_rank(int_l_mm[ribo_genes$Symbol_mm], max_rank = length(genes_mm))
+keep_exps <- intersect(rownames(rank_mat), names(which(msr_hg[gene_hg, ] == 1)))
+
+set.seed(77)
+
+sample_genes <- lapply(keep_exps, function(x) {
+  msr_genes <- msr_hg[msr_hg[, x] == 1, x]
+  names(sample(msr_genes, 1))
+})
 
 
-med_ribo_srank_df <- rbind(mat_to_df(diag_to_na(med_ribo_srank_hg)),
-                           mat_to_df(diag_to_na(med_ribo_srank_mm)))
+sample_mat <- lapply(1:length(keep_exps), function(x) {
+  load_agg_mat_list(ids = keep_exps[x], sub_genes = sample_genes[[x]], genes = pc_hg$Symbol)[[1]]
+})
+sample_mat <- do.call(cbind, sample_mat)
+colnames(sample_mat) <- paste0(keep_exps, "_", unlist(sample_genes))
 
 
-hist(med_ribo_srank_df$Value, breaks = 100)
+sample_topk <- colwise_topk_intersect(sample_mat)
+sample_df <- mat_to_df(sample_topk, symmetric = TRUE, value_name = "Topk")
 
 
-# Inspecting example of RPL19 being the top hit between datasets
+plot_df <- data.frame(
+  Topk = c(gene_sim_df$Topk, sample_df$Topk),
+  Group = c(rep(gene_hg, length(gene_sim_df$Topk)), rep("Sampled", length(sample_df$Topk)))
+)
 
 
-gene_topk_rank_hg <- which_ranked_intersect(
-  rank_vec = agg_hg$GSE202352[, "RPL19"],
-  rank_mat = agg_hg$GSE180928,
-  gene = "RPL19",
-  k = 1000,
-  ncores = ncore)
+ggplot(plot_df, aes(x = Topk, fill = Group)) +
+  geom_density(alpha = 0.8) +
+  # geom_histogram(position = "stack", bins = 100) + 
+  theme_classic()
 
-
-gene_topk_rank_hg$List %>% head
-
-
-# Inspect TFs
-
-
-med_tf_srank_hg <- med_standard_rank(int_l_hg[tfs_hg], max_rank = length(genes_hg))
-med_tf_srank_mm <- med_standard_rank(int_l_mm[tfs_mm], max_rank = length(genes_mm))
-
-
-med_tf_srank_df <- rbind(mat_to_df(diag_to_na(med_tf_srank_hg)),
-                         mat_to_df(diag_to_na(med_tf_srank_mm)))
-
-
-hist(med_tf_srank_df$Value, breaks = 10)
 
 
 
@@ -240,3 +265,41 @@ hist(med_tf_srank_df$Value, breaks = 10)
 #          fontsize = 20,
 #          cellwidth = 50,
 #          cellheight = 50)
+
+
+# Collapse the ribosomal gene rankings: First standardize each rank (0 worst, 
+# 1 best), and then take the median of [exp1, exp2] ranks across ribo genes.
+
+# med_standard_rank <- function(mat_l, max_rank) {
+#   
+#   # because current ranks are 1=best to max_rank=worst, reverse for standard
+#   mat_l <- lapply(mat_l, function(x) (max_rank + 1 - x) / max_rank)
+#   med_mat <- apply(simplify2array(mat_l), 1:2, median)
+#   return(med_mat)
+# }
+# 
+# 
+# med_ribo_srank_hg <- med_standard_rank(int_l_hg[ribo_genes$Symbol_hg], max_rank = length(genes_hg))
+# med_ribo_srank_mm <- med_standard_rank(int_l_mm[ribo_genes$Symbol_mm], max_rank = length(genes_mm))
+# 
+# 
+# med_ribo_srank_df <- rbind(mat_to_df(diag_to_na(med_ribo_srank_hg)),
+#                            mat_to_df(diag_to_na(med_ribo_srank_mm)))
+# 
+# 
+# hist(med_ribo_srank_df$Value, breaks = 100)
+
+
+
+# # Inspect TFs
+# 
+# 
+# med_tf_srank_hg <- med_standard_rank(int_l_hg[tfs_hg], max_rank = length(genes_hg))
+# med_tf_srank_mm <- med_standard_rank(int_l_mm[tfs_mm], max_rank = length(genes_mm))
+# 
+# 
+# med_tf_srank_df <- rbind(mat_to_df(diag_to_na(med_tf_srank_hg)),
+#                          mat_to_df(diag_to_na(med_tf_srank_mm)))
+# 
+# 
+# hist(med_tf_srank_df$Value, breaks = 10)
