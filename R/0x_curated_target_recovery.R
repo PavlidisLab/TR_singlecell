7c615de9dc750c0db67b20e2602fd5f65336f851
Alexander-Morin <alexm.14@outlook.com>
2023-09-04 12:45:49 -0700

    init script for curated target recovery

---
+++ R/0x_curated_target_recovery.R
@@ -0,0 +1,49 @@
+## TODO: point to missing genes
+## TODO: missing genes excluded from rank df makes a difference?
+## TODO: common process for curated and top k evidence for get_rank_df
+## TODO: remove tf?
+## -----------------------------------------------------------------------------
+
+library(tidyverse)
+library(data.table)
+library(WGCNA)
+library(parallel)
+library(pheatmap)
+library(RColorBrewer)
+library(cowplot)
+source("R/utils/functions.R")
+source("R/utils/vector_comparison_functions.R")
+source("R/utils/plot_functions.R")
+source("R/00_config.R")
+
+# Table of assembled scRNA-seq datasets
+sc_meta <- read.delim(sc_meta_path, stringsAsFactors = FALSE)
+
+# IDs for scRNA-seq datasets
+ids_hg <- filter(sc_meta, Species == "Human")$ID
+ids_mm <- filter(sc_meta, Species == "Mouse")$ID
+
+# Protein coding genes 
+pc_hg <- read.delim(ens_hg_path, stringsAsFactors = FALSE)
+pc_mm <- read.delim(ens_mm_path, stringsAsFactors = FALSE)
+pc_ortho <- read.delim(pc_ortho_path)
+
+# Transcription Factors
+tfs_hg <- filter(read.delim(tfs_hg_path, stringsAsFactors = FALSE), Symbol %in% pc_hg$Symbol)
+tfs_mm <- filter(read.delim(tfs_mm_path, stringsAsFactors = FALSE), Symbol %in% pc_mm$Symbol)
+tfs_mm <- distinct(tfs_mm, Symbol, .keep_all = TRUE)
+
+# Measurement matrices used for filtering when a gene was never expressed
+msr_hg <- readRDS(msr_mat_hg_path)
+msr_mm <- readRDS(msr_mat_mm_path)
+
+# For each dataset, load the subset gene x TF aggregation matrix 
+agg_tf_hg <- load_or_generate_agg(path = agg_tf_hg_path, ids = ids_hg, genes = pc_hg$Symbol, sub_genes = tfs_hg$Symbol)
+agg_tf_mm <- load_or_generate_agg(path = agg_tf_mm_path, ids = ids_mm, genes = pc_mm$Symbol, sub_genes = tfs_mm$Symbol)
+
+# Saved list RDS of the summaries
+rank_tf_hg <- readRDS(rank_tf_hg_path)
+rank_tf_mm <- readRDS(rank_tf_mm_path)
+
+
+
