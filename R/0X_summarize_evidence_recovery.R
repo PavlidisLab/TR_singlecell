7e827fffa3b27be28e56d448ac4a6eccdf5e0044
Alexander-Morin <alexm.14@outlook.com>
2023-09-14 10:34:16 -0700

    load actual data; generic hist plot; functions for summarizing ROC perf; rm old

--- R/0X_summarize_evidence_recovery.R
+++ R/0X_summarize_evidence_recovery.R
@@ -66,7 +66,7 @@ bind_model <- readRDS(bind_model_path)
 
 # List of AUROC/AUPRC for TF lists ability to recover curated targets
 unibind_perf_hg <- readRDS(unibind_recover_curated_hg_path)
-unibind_perf_mm <- readRDS(unibind_recover_curated_hg_path)
+unibind_perf_mm <- readRDS(unibind_recover_curated_mm_path)
 coexpr_perf_hg <- readRDS(coexpr_recover_curated_hg_path)
 coexpr_perf_mm <- readRDS(coexpr_recover_curated_mm_path)
 
@@ -87,7 +87,7 @@ join_perf_df <- function(coexpr_l, unibind_l) {
   
   keep_unibind <- names(which(unlist(lapply(unibind_l, function(x) "Perf_df" %in% names(x)))))
   unibind_df <- do.call(rbind, lapply(unibind_l[keep_unibind], `[[`, "Perf_df"))
-
+  
   left_join(coexpr_df, unibind_df,
             by = c("Symbol", "N_targets"),
             suffix = c("_coexpr", "_unibind"))
@@ -111,11 +111,129 @@ summary(Filter(is.numeric, perf_sub_hg))
 summary(Filter(is.numeric, perf_sub_mm))
 
 
-ggplot(perf_sub_hg, aes(x = Percentile_observed_PR)) +
-  geom_histogram(bins = 100) +
-  ggtitle("Human") +
-  xlab("AUPRC percentile observed greater than null") +
-  ylab("Frequency") +
+# Minimum count of curated targets common to unibind and coexpr
+
+perf_common_hg <- filter(perf_sub_hg, !is.na(AUPRC_coexpr) & !is.na(AUPRC_unibind))
+perf_common_mm <- filter(perf_sub_mm, !is.na(AUPRC_coexpr) & !is.na(AUPRC_unibind))
+
+
+summary(Filter(is.numeric, perf_common_hg))
+summary(Filter(is.numeric, perf_common_mm))
+
+
+# Proportion with notably good or bad performance
+
+stat_cols <- c(
+  "AUPRC_percentile_observed_coexpr",
+  "AUPRC_percentile_observed_unibind",
+  "AUROC_percentile_observed_coexpr",
+  "AUROC_percentile_observed_unibind"
+)
+
+
+# TODO: function that returns proportion of data in percentile bins
+
+
+
+top_and_bottom_prop <- function(df, stat_cols) {
+  
+  lapply(df[, stat_cols], function(col) {
+    
+    col <- col[!is.na(col)]
+    n <- length(col)
+    
+    round(c(
+      N = n,
+      Eq1 = sum(col == 1) / n,
+      Gt09 = sum(col > 0.9) / n,
+      Lt01 = sum(col < 0.1) / n
+      ), 3)
+  })
+}
+
+
+top_and_bottom_prop(perf_sub_hg, stat_cols)
+top_and_bottom_prop(perf_sub_mm, stat_cols)
+
+top_and_bottom_prop(perf_common_hg, stat_cols)
+top_and_bottom_prop(perf_common_mm, stat_cols)
+
+
+
+
+
+
+
+
+plot_hist <- function(df, stat_col, title = NULL, xlab = NULL) {
+  
+  if (is.null(xlab)) xlab <- stat_col
+  
+  ggplot(df, aes(x = !!sym(stat_col))) +
+    geom_histogram(bins = 100) +
+    ggtitle(title) +
+    xlab(xlab) +
+    ylab("Frequency") +
+    theme_classic() +
+    theme(axis.text = element_text(size = 20),
+          axis.title = element_text(size = 20),
+          plot.title = element_text(size = 20))
+  
+}
+
+
+# This uses all TFs for coexpr and unibind
+
+stat <- "AUPRC"
+plot_df_hg <- perf_sub_hg  # perf_common_hg 
+plot_df_mm <- perf_sub_mm  # perf_common_mm
+
+p_auprc_hist_hg <- list(
+  plot_hist(plot_df_hg, stat_col = paste0(stat, "_percentile_observed_coexpr")),
+  plot_hist(plot_df_hg, stat_col = paste0(stat, "_percentile_observed_unibind")))
+
+
+p_auprc_hist_mm <- list(
+  plot_hist(plot_df_mm, stat_col = paste0(stat, "_percentile_observed_coexpr")),
+  plot_hist(plot_df_mm, stat_col = paste0(stat, "_percentile_observed_unibind")))
+
+
+plot_grid(
+  plot_grid(plotlist = p_auprc_hist_hg), 
+  plot_grid(plotlist = p_auprc_hist_mm),
+  nrow = 2)
+
+
+
+
+
+
+# Demo a single TF's performance relative to null distn
+
+
+tf_hg <- "ASCL1"
+tf_mm <- "Ascl1"
+
+xmin <- 0.01
+xmax <- 0.05
+
+null <- unlist(lapply(unibind_perf_hg[[tf_hg]]$Null, `[[`, stat))
+obs <- unibind_perf_hg[[tf_hg]]$Perf_df[[stat]]
+hist(null, xlim = c(xmin, xmax), main = tf_hg, breaks = 100)
+abline(v = obs, col = "red")
+
+
+null <- unlist(lapply(unibind_perf_mm[[tf_mm]]$Null, `[[`, stat))
+obs <- unibind_perf_mm[[tf_mm]]$Perf_df[[stat]]
+hist(null, xlim = c(xmin, xmax), main = tf_mm, breaks = 100)
+abline(v = obs, col = "red")
+
+
+
+# 
+
+ggplot(perf_common_hg, aes(x = AUPRC_percentile_observed_coexpr, y = AUPRC_percentile_observed_unibind)) +
+  geom_point(shape = 21, size = 2.4) +
   theme_classic() +
   theme(axis.text = element_text(size = 20),
         axis.title = element_text(size = 20),
@@ -123,75 +241,49 @@ ggplot(perf_sub_hg, aes(x = Percentile_observed_PR)) +
 
 
 
-
-ggplot(perf_sub_hg, aes(x = Percentile_observed_ROC)) +
-  geom_histogram(bins = 100) +
-  ggtitle("Human") +
-  xlab("AUROC percentile observed greater than null") +
-  ylab("Frequency") +
-  theme_classic() +
-  theme(axis.text = element_text(size = 20),
-        axis.title = element_text(size = 20),
-        plot.title = element_text(size = 20))
-
-
-
-
-hist(perf_sub_hg$Percentile_observed_PR, breaks = 100)
-hist(perf_sub_hg$Percentile_observed_ROC, breaks = 100)
-
-hist(perf_sub_mm$Percentile_observed_PR, breaks = 100)
-hist(perf_sub_mm$Percentile_observed_ROC, breaks = 100)
-
-
-sum(perf_sub_hg$Percentile_observed_PR == 1) / nrow(perf_sub_hg)
-sum(perf_sub_mm$Percentile_observed_PR == 1) / nrow(perf_sub_mm)
-
-sum(perf_sub_hg$Percentile_observed_ROC == 1) / nrow(perf_sub_hg)
-sum(perf_sub_mm$Percentile_observed_ROC == 1) / nrow(perf_sub_mm)
-
-sum(perf_sub_hg$Percentile_observed_PR > 0.9) / nrow(perf_sub_hg)
-sum(perf_sub_mm$Percentile_observed_PR > 0.9) / nrow(perf_sub_mm)
-
-sum(perf_sub_hg$Percentile_observed_ROC > 0.9) / nrow(perf_sub_hg)
-sum(perf_sub_mm$Percentile_observed_ROC > 0.9) / nrow(perf_sub_mm)
-
-sum(perf_sub_hg$Percentile_observed_PR < 0.1) / nrow(perf_sub_hg)
-sum(perf_sub_mm$Percentile_observed_PR < 0.1) / nrow(perf_sub_mm)
-
-sum(perf_sub_hg$Percentile_observed_ROC < 0.1) / nrow(perf_sub_hg)
-sum(perf_sub_mm$Percentile_observed_ROC < 0.1) / nrow(perf_sub_mm)
+# plot(perf_common_hg$AUPRC_percentile_observed_coexpr, perf_common_hg$AUPRC_percentile_observed_unibind)
+# cor(perf_common_hg$AUPRC_percentile_observed_coexpr, perf_common_hg$AUPRC_percentile_observed_unibind, use = "pairwise.complete.obs", method = "spearman")
+# 
+# plot(perf_common_hg$AUROC_percentile_observed_coexpr, perf_common_hg$AUROC_percentile_observed_unibind)
+# cor(perf_common_hg$AUROC_percentile_observed_coexpr, perf_common_hg$AUROC_percentile_observed_unibind, use = "pairwise.complete.obs", method = "spearman")
+# 
+# plot(perf_common_mm$AUPRC_percentile_observed_coexpr, perf_common_mm$AUPRC_percentile_observed_unibind)
+# cor(perf_common_mm$AUPRC_percentile_observed_coexpr, perf_common_mm$AUPRC_percentile_observed_unibind, use = "pairwise.complete.obs", method = "spearman")
+# 
+# plot(perf_common_mm$AUPRC_percentile_observed_coexpr, perf_common_mm$AUPRC_percentile_observed_unibind)
+# cor(perf_common_mm$AUPRC_percentile_observed_coexpr, perf_common_mm$AUPRC_percentile_observed_unibind, use = "pairwise.complete.obs", method = "spearman")
 
 
 
-# Demo a single TF
+top_both_hg <- perf_common_hg %>% 
+  filter(AUPRC_percentile_observed_coexpr > 0.9 &
+           AUPRC_percentile_observed_unibind > 0.9)
 
 
-tf_hg <- "SREBF2"
-tf_mm <- "Sp1"
-
-# hist(curated_auprc_hg[[tf_hg]]$Null, xlim = c(0, curated_auprc_hg[[tf_hg]]$Perf_df$AUC * 1.5), main = tf_hg)
-hist(curated_auprc_hg[[tf_hg]]$Null, main = tf_hg)
-abline(v = curated_auprc_hg[[tf_hg]]$Perf_df$AUC, col = "red")
+top_both_mm <- perf_common_mm %>% 
+  filter(AUPRC_percentile_observed_coexpr > 0.9 &
+           AUPRC_percentile_observed_unibind > 0.9)
 
 
-# hist(curated_auprc_mm[[tf_mm]]$Null, xlim = c(0, curated_auprc_mm[[tf_mm]]$Perf_df$AUC * 1.5), main = tf_mm)
-hist(curated_auprc_mm[[tf_mm]]$Null, main = tf_mm)
-abline(v = curated_auprc_mm[[tf_mm]]$Perf_df$AUC, col = "red")
+top_ortho <- intersect(top_both_hg$Symbol, str_to_upper(top_both_mm$Symbol))
 
 
-perf_sub_hg %>% 
-  mutate(Diff = abs(Percentile_observed_PR - Percentile_observed_ROC)) %>% 
-  arrange(desc(Diff)) %>% 
-  head()
 
 
-ggplot(perf_sub_hg, aes(x = Percentile_observed_PR, y = Percentile_observed_ROC)) +
-  geom_point() +
-  theme_classic() +
-  theme(axis.text = element_text(size = 20),
-        axis.title = element_text(size = 20),
-        plot.title = element_text(size = 20))
+diff_hg <- perf_common_hg %>%
+  mutate(Diff_AUPRC = abs(
+    AUPRC_percentile_observed_coexpr - AUPRC_percentile_observed_unibind
+  )) 
+# %>% filter(Diff_AUPRC > 0.8)
+
+
+diff_mm <- perf_common_mm %>%
+  mutate(Diff_AUPRC = abs(
+    AUPRC_percentile_observed_coexpr - AUPRC_percentile_observed_unibind
+  )) 
+# %>% filter(Diff_AUPRC > 0.8)
+
+
 
 
 
@@ -239,189 +331,49 @@ ggplot(perf_sub_hg, aes(x = Percentile_observed_PR, y = Percentile_observed_ROC)
 
 
 
-tt <- summarize_obs_and_null_auc(tf = tf,
-                                 score_vec = score_vec,
-                                 label_vec = label_vec,
-                                 label_all = targets_curated_mm,
-                                 n_samps = 1000,
-                                 ncores = 8)
-
-
-
-
-tt <- curated_obs_and_null_auc(tf = tf,
-                               rank_df = rank_tf_mm[[tf]],
-                               score_col = "Avg_RSR",
-                               curated_df = curated,
-                               label_all = targets_curated_mm,
-                               pc_df = pc_mm,
-                               species = "Mouse",
-                               n_samps = 1000,
-                               ncores = 8)
-
-
-tt <- curated_obs_and_null_auc_list(tfs = tfs_curated_mm[6:8],
-                                    rank_l = rank_tf_mm,
-                                    score_col = "Avg_RSR",
-                                    curated_df = curated,
-                                    label_all = targets_curated_mm,
-                                    pc_df = pc_mm,
-                                    species = "Mouse",
-                                    n_samps = 1000,
-                                    ncores = 8,
-                                    verbose = TRUE)
-
-
-
-save_curated_auc_list(path = "tt.RDS",
-                      tfs = tfs_curated_mm[6:8],
-                      rank_l = rank_tf_mm,
-                      score_col = "Avg_RSR",
-                      curated_df = curated,
-                      label_all = targets_curated_mm,
-                      pc_df = pc_mm,
-                      species = "Mouse",
-                      n_samps = 1000,
-                      ncores = 8,
-                      verbose = TRUE,
-                      force_resave = TRUE)
-
-
-##
-
-
-
-
-unibind_auprc_hg <- readRDS(unibind_auprc_hg_path)
-unibind_auroc_hg <- readRDS(unibind_auroc_hg_path)
-unibind_auprc_mm <- readRDS(unibind_auprc_mm_path)
-unibind_auroc_mm <- readRDS(unibind_auroc_mm_path)
-
-
-join_perf_df <- function(auprc_l, auroc_l) {
-  
-  auprc_df <- do.call(rbind, lapply(auprc_l, `[[`, "Perf_df"))
-  auroc_df <- do.call(rbind, lapply(auroc_l, `[[`, "Perf_df"))
-  
-  left_join(auprc_df, auroc_df,
-            by = c("Symbol", "N_targets"),
-            suffix = c("_PR", "_ROC"))
-}
-
-
-unibind_df_hg <- join_perf_df(unibind_auprc_hg, unibind_auroc_hg)
-unibind_df_mm <- join_perf_df(unibind_auprc_mm, unibind_auroc_mm)
-
-unibind_df_sub_hg <- filter(unibind_df_hg, N_targets >= 5)
-unibind_df_sub_mm <- filter(unibind_df_mm, N_targets >= 5)
-
-summary(Filter(is.numeric, unibind_df_sub_hg))
-summary(Filter(is.numeric, unibind_df_sub_mm))
-
-
-ggplot(unibind_df_sub_hg, aes(x = Percentile_observed_PR)) +
-  geom_histogram(bins = 100) +
-  ggtitle("Human") +
-  xlab("AUPRC percentile observed greater than null") +
-  ylab("Frequency") +
-  theme_classic() +
-  theme(axis.text = element_text(size = 20),
-        axis.title = element_text(size = 20),
-        plot.title = element_text(size = 20))
-
-
-
-
-ggplot(unibind_df_sub_hg, aes(x = Percentile_observed_ROC)) +
-  geom_histogram(bins = 100) +
-  ggtitle("Human") +
-  xlab("AUROC percentile observed greater than null") +
-  ylab("Frequency") +
-  theme_classic() +
-  theme(axis.text = element_text(size = 20),
-        axis.title = element_text(size = 20),
-        plot.title = element_text(size = 20))
-
-
-
-
-### from unibind script
-
-
-
-curated_auprc_hg_path <- "/space/scratch/amorin/R_objects/curated_auprc_hg.RDS"
-curated_auprc_mm_path <- "/space/scratch/amorin/R_objects/curated_auprc_mm.RDS"
-curated_auroc_hg_path <- "/space/scratch/amorin/R_objects/curated_auroc_hg.RDS"
-curated_auroc_mm_path <- "/space/scratch/amorin/R_objects/curated_auroc_mm.RDS"
-
-
-curated_auprc_hg <- readRDS(curated_auprc_hg_path)
-curated_auroc_hg <- readRDS(curated_auroc_hg_path)
-curated_auprc_mm <- readRDS(curated_auprc_mm_path)
-curated_auroc_mm <- readRDS(curated_auroc_mm_path)
-
-
-curated_df_hg <- join_perf_df(curated_auprc_hg, curated_auroc_hg)
-curated_df_mm <- join_perf_df(curated_auprc_mm, curated_auroc_mm)
-
-
-df_hg <- left_join(curated_df_hg, 
-                   unibind_df_hg,
-                   by = c("Symbol", "N_targets"),
-                   suffix = c("_coexpr", "_binding"))
-
-
-
-df_mm <- left_join(curated_df_mm, 
-                   unibind_df_mm,
-                   by = c("Symbol", "N_targets"),
-                   suffix = c("_coexpr", "_binding"))
-
-
-
-plot(df_hg$Percentile_observed_PR_coexpr, df_hg$Percentile_observed_PR_binding)
-cor(df_hg$Percentile_observed_PR_coexpr, df_hg$Percentile_observed_PR_binding, use = "pairwise.complete.obs", method = "spearman")
-
-
-plot(df_mm$Percentile_observed_PR_coexpr, df_mm$Percentile_observed_PR_binding)
-cor(df_mm$Percentile_observed_PR_coexpr, df_mm$Percentile_observed_PR_binding, use = "pairwise.complete.obs", method = "spearman")
-
-
-df_hg %>% 
-  filter(N_targets >= 5 &
-           Percentile_observed_PR_binding > 0.9 &
-           Percentile_observed_PR_coexpr > 0.9)
-
-
-df_mm %>% 
-  filter(N_targets >= 5 &
-           Percentile_observed_PR_binding > 0.9 &
-           Percentile_observed_PR_coexpr > 0.9)
-
-
-
-diff_hg <- df_hg %>%
-  filter(N_targets >= 5) %>%
-  mutate(Diff_PR = abs(
-    Percentile_observed_PR_binding - Percentile_observed_PR_coexpr
-  )) %>%
-  filter(Diff_PR > 0.8)
-
-
-
-
-diff_mm <- df_mm %>%
-  filter(N_targets >= 5) %>%
-  mutate(Diff_PR = abs(
-    Percentile_observed_PR_binding - Percentile_observed_PR_coexpr
-  )) %>%
-  filter(Diff_PR > 0.8)
-
-
-
-diff_ortho <- intersect(diff_hg$Symbol, str_to_upper(diff_mm$Symbol))
-
-
-
-filter(df_hg, Symbol %in% diff_ortho)
-filter(df_mm, Symbol %in% str_to_title(diff_ortho))
+# tt <- summarize_obs_and_null_auc(tf = tf,
+#                                  score_vec = score_vec,
+#                                  label_vec = label_vec,
+#                                  label_all = targets_curated_mm,
+#                                  n_samps = 1000,
+#                                  ncores = 8)
+# 
+# 
+# 
+# 
+# tt <- curated_obs_and_null_auc(tf = tf,
+#                                rank_df = rank_tf_mm[[tf]],
+#                                score_col = "Avg_RSR",
+#                                curated_df = curated,
+#                                label_all = targets_curated_mm,
+#                                pc_df = pc_mm,
+#                                species = "Mouse",
+#                                n_samps = 1000,
+#                                ncores = 8)
+# 
+# 
+# tt <- curated_obs_and_null_auc_list(tfs = tfs_curated_mm[6:8],
+#                                     rank_l = rank_tf_mm,
+#                                     score_col = "Avg_RSR",
+#                                     curated_df = curated,
+#                                     label_all = targets_curated_mm,
+#                                     pc_df = pc_mm,
+#                                     species = "Mouse",
+#                                     n_samps = 1000,
+#                                     ncores = 8,
+#                                     verbose = TRUE)
+# 
+# 
+# 
+# save_curated_auc_list(path = "tt.RDS",
+#                       tfs = tfs_curated_mm[6:8],
+#                       rank_l = rank_tf_mm,
+#                       score_col = "Avg_RSR",
+#                       curated_df = curated,
+#                       label_all = targets_curated_mm,
+#                       pc_df = pc_mm,
+#                       species = "Mouse",
+#                       n_samps = 1000,
+#                       ncores = 8,
+#                       verbose = TRUE,
+#                       force_resave = TRUE)
