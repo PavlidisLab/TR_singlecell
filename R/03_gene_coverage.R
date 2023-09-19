14748ba3339a6517bc882e27e7d8145d9fc6b8bc
Alexander-Morin <alexm.14@outlook.com>
2023-09-19 16:58:17 -0700

    minor doc with CPM update

--- R/03_gene_coverage.R
+++ R/03_gene_coverage.R
@@ -83,9 +83,9 @@ if (!file.exists(msr_mat_mm_path)) {
 
 
 # Get the average/proportion of gene measurement across experiments
-# Human: 450 genes are never measured and 5,119 genes always measured
+# Human: 450 genes are never measured and 5,120 genes always measured
 # Mouse: 882 genes are never measured and 838 genes are always measured
-# Ortho: 627 genes measured in every mouse and human dataset
+# Ortho: 628 genes measured in every mouse and human dataset
 # TFs: In both species TF genes show small trend of being measured more than non-TF genes 
 # ------------------------------------------------------------------------------
 
@@ -145,8 +145,8 @@ tf_wilx_count <- gene_msr_df %>%
 
 
 # Look at experiment-wise gene coverage
-# Human: HPA max coverage at 18299 genes, GSE85241 min at 8953
-# Mouse: TabulaMuris max coverage at 17978 genes, GSE160193 min at 1988 genes.
+# Human: HPA max coverage at 18299 genes, GSE85241 min at 8952
+# Mouse: TabulaMuris max coverage at 17977 genes, GSE160193 min at 1988 genes.
 # ------------------------------------------------------------------------------
 
 
