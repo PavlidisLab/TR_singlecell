d3835023ad81372ec89e3c7de242cc0d3991b748
Alexander-Morin <alexm.14@outlook.com>
2023-10-10 21:09:56 -0700

    add bottom k to comparison; plotting; ensure matching ranks

--- R/tests/compare_lognorm_vs_CPM.R
+++ R/tests/compare_lognorm_vs_CPM.R
@@ -9,7 +9,7 @@ source("R/utils/vector_comparison_functions.R")
 source("R/utils/plot_functions.R")
 source("R/00_config.R")
 
-k <- 1000
+k <- 200
 
 # Table of assembled scRNA-seq datasets
 sc_meta <- read.delim(sc_meta_path, stringsAsFactors = FALSE)
@@ -43,6 +43,9 @@ stopifnot(identical(names(rank_cpm_mm), names(rank_ln_mm)))
 stopifnot(identical(rank_cpm_hg[[1]]$Symbol, rank_ln_hg[[1]]$Symbol))
 stopifnot(identical(rank_cpm_mm[[1]]$Symbol, rank_ln_mm[[1]]$Symbol))
 
+# Focus on human aggregate lists due to size
+agg_cpm_hg <- readRDS("/space/scratch/amorin/R_objects/TF_agg_mat_list_hg.RDS")
+agg_ln_hg <- readRDS("/space/scratch/amorin/R_objects/TF_agg_mat_list_hg_lognorm.RDS")
 
 
 # 1. Differences in binary measurement
@@ -69,7 +72,7 @@ msr_diff_mm <- get_msr_diff(msr_cpm_mm, msr_ln_mm)
 
 
 
-# 2. 
+# 2. Examining TF similarity
 # ------------------------------------------------------------------------------
 
 
@@ -93,7 +96,6 @@ get_summary_df <- function(sim_l, msr_mat) {
   names(df_l) <- stats
   return(df_l)
 }
-
 
 
 
@@ -136,26 +138,20 @@ plot_hist(sim_df_mm, stat_col = "Diff")
 
 
 
-# Most diff
-
-diff_rank_df <- left_join(rank_cpm_hg$YBX1, 
-                          rank_ln_hg$YBX1,
-                          by = "Symbol",
-                          suffix = c("_CPM", "_LN")) %>% 
-  mutate(Diff = Rank_RSR_CPM - Rank_RSR_LN)
+# 3) Examining aggregate rankings
+# ------------------------------------------------------------------------------
 
 
 # Cor of aggregate ranking
 
-
-get_rank_cor_df <- function(cpm_l, lognorm_l, ncore = 1) {
+get_rank_cor_df <- function(cpm_l, lognorm_l, ncores = 1) {
   
   symbols <- intersect(names(cpm_l), names(lognorm_l))
   
   cor_l <- mclapply(symbols, function(x) {
     cor(cpm_l[[x]]$Avg_RSR, lognorm_l[[x]]$Avg_RSR, 
         use = "pairwise.complete.obs", method = "spearman")
-  }, mc.cores = ncore)
+  }, mc.cores = ncores)
   
   cor_df <- data.frame(Symbol = symbols, Cor = unlist(cor_l))
   
@@ -170,3 +166,106 @@ rank_cor_mm <- get_rank_cor_df(rank_cpm_mm, rank_ln_mm, ncore)
 plot_hist(rank_cor_hg, stat_col = "Cor")
 plot_hist(rank_cor_mm, stat_col = "Cor")
 
+
+# Top/bottom k between rankings
+
+get_topk_df <- function(cpm_l, lognorm_l, ncores = 1) {
+  
+  symbols <- intersect(names(cpm_l), names(lognorm_l))
+  
+  topk_l <- mclapply(symbols, function(x) {
+    
+    vec1 <- setNames(cpm_l[[x]]$Avg_RSR, cpm_l[[x]]$Symbol)
+    vec2 <- setNames(lognorm_l[[x]]$Avg_RSR, lognorm_l[[x]]$Symbol)
+    
+    top_k200 <- topk_intersect(topk_sort(vec1, k = 200), 
+                               topk_sort(vec2, k = 200))
+    
+    top_k1000 <- topk_intersect(topk_sort(vec1, k = 1000), 
+                                topk_sort(vec2, k = 1000))
+    
+    bottom_k200 <- topk_intersect(topk_sort(vec1, k = 200, decreasing = FALSE), 
+                                  topk_sort(vec2, k = 200, decreasing = FALSE))
+    
+    bottom_k1000 <- topk_intersect(topk_sort(vec1, k = 1000, decreasing = FALSE),
+                                   topk_sort(vec2, k = 1000, decreasing = FALSE))
+    
+    data.frame(
+      Symbol = x,
+      Top_K200 = top_k200,
+      Top_K1000 = top_k1000,
+      Bottom_K200 = bottom_k200,
+      Bottom_K1000 = bottom_k1000)
+
+  }, mc.cores = ncores)
+  
+  topk_df <- do.call(rbind, topk_l)
+  return(topk_df)
+}
+
+
+topk_df_hg <- get_topk_df(rank_cpm_hg, rank_ln_hg, ncores = ncore)
+topk_df_mm <- get_topk_df(rank_cpm_mm, rank_ln_mm, ncores = ncore)
+
+
+
+qplot(topk_df_hg, xvar = "Top_K200", yvar = "Top_K1000")
+qplot(topk_df_mm, xvar = "Top_K200", yvar = "Top_K1000")
+qplot(topk_df_hg, xvar = "Top_K200", yvar = "Top_K1000")
+qplot(topk_df_mm, xvar = "Top_K200", yvar = "Top_K1000")
+
+qplot(topk_df_hg, xvar = "Top_K200", yvar = "Bottom_K200")
+qplot(topk_df_mm, xvar = "Top_K200", yvar = "Bottom_K200")
+qplot(topk_df_hg, xvar = "Top_K1000", yvar = "Bottom_K1000")
+qplot(topk_df_mm, xvar = "Top_K1000", yvar = "Bottom_K1000")
+
+plot_hist(topk_df_hg, stat_col = "Top_K200")
+plot_hist(topk_df_mm, stat_col = "Top_K200")
+plot_hist(topk_df_hg, stat_col = "Top_K1000")
+plot_hist(topk_df_mm, stat_col = "Top_K1000")
+
+plot_hist(topk_df_hg, stat_col = "Bottom_K200")
+plot_hist(topk_df_mm, stat_col = "Bottom_K200")
+plot_hist(topk_df_hg, stat_col = "Bottom_K1000")
+plot_hist(topk_df_mm, stat_col = "Bottom_K1000")
+
+
+
+# Examine difference in ranks for a given TF
+
+gene <- "PAX6"
+
+diff_rank_df <- left_join(rank_cpm_hg[[gene]], 
+                          rank_ln_hg[[gene]],
+                          by = "Symbol",
+                          suffix = c("_CPM", "_LN")) %>% 
+  mutate(Diff = Rank_RSR_CPM - Rank_RSR_LN)
+
+
+
+qplot(diff_rank_df, xvar = "Avg_RSR_CPM", yvar = "Avg_RSR_LN")
+
+
+# Explore at level of aggregate matrices
+
+mat_cpm <- subset_to_measured(gene_vec_to_mat(agg_cpm_hg, gene), msr_cpm_hg, gene)
+mat_ln <- subset_to_measured(gene_vec_to_mat(agg_ln_hg, gene), msr_ln_hg, gene)
+
+comsr <- lapply(rownames(mat_cpm), function(y) {
+  mat_cpm[gene, colnames(mat_cpm)] & msr_cpm_hg[y, colnames(mat_cpm)]
+})
+comsr <- do.call(rbind, comsr)
+rownames(comsr) <- rownames(mat_cpm)
+
+# When a TF-gene was not co-measured, impute to the median NA
+med_cpm <- median(mat_cpm, na.rm = TRUE)
+med_ln <- median(mat_ln, na.rm = TRUE)
+
+mat_cpm[comsr == FALSE] <- med_cpm
+mat_ln[comsr == FALSE] <- med_ln
+
+arrange(rank_cpm_hg[[gene]], Rank_RSR) %>% head
+arrange(rank_ln_hg[[gene]], Rank_RSR) %>% head
+
+head(sort(rowMeans(mat_cpm), decreasing = TRUE), 10)
+head(sort(rowMeans(mat_ln), decreasing = TRUE), 10)
