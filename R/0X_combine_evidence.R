30a553defdd3b620b17231ddd7a380dbf301416e
Alexander-Morin <alexm.14@outlook.com>
2023-11-26 16:13:26 -0800

    init update of data

--- R/0X_combine_evidence.R
+++ R/0X_combine_evidence.R
@@ -1,4 +1,44 @@
-## TODO:
+## 
 ## -----------------------------------------------------------------------------
+
+library(tidyverse)
+library(parallel)
+library(cowplot)
+library(ggrepel)
+source("R/utils/functions.R")
+source("R/utils/vector_comparison_functions.R")
+source("R/utils/plot_functions.R")
+source("R/00_config.R")
+
+k <- 200
+
+# Table of assembled scRNA-seq datasets
+sc_meta <- read.delim(sc_meta_path, stringsAsFactors = FALSE)
+
+# IDs for scRNA-seq datasets
+ids_hg <- filter(sc_meta, Species == "Human")$ID
+ids_mm <- filter(sc_meta, Species == "Mouse")$ID
+
+# Protein coding genes 
+pc_hg <- read.delim(ens_hg_path, stringsAsFactors = FALSE)
+pc_mm <- read.delim(ens_mm_path, stringsAsFactors = FALSE)
+pc_ortho <- read.delim(pc_ortho_path)
+
+# Ribosomal genes
+sribo_hg <- read.table("/space/grp/amorin/Metadata/HGNC_human_Sribosomal_genes.csv", stringsAsFactors = FALSE, skip = 1, sep = ",", header = TRUE)
+lribo_hg <- read.table("/space/grp/amorin/Metadata/HGNC_human_Lribosomal_genes.csv", stringsAsFactors = FALSE, skip = 1, sep = ",", header = TRUE)
+ribo_genes <- filter(pc_ortho, Symbol_hg %in% c(sribo_hg$Approved.symbol, lribo_hg$Approved.symbol))
+
+# Saved list RDS of the ranks
+rank_tf_hg <- readRDS(rank_tf_hg_path)
+rank_tf_mm <- readRDS(rank_tf_mm_path)
+rank_ribo_hg <- readRDS(rank_ribo_hg_path)
+rank_ribo_mm <- readRDS(rank_ribo_mm_path)
+
+# Average bind scores
+collection <- "Permissive"
+stopifnot(collection %in% c("Robust", "Permissive"))
+bind_summary_path <- paste0("/space/scratch/amorin/R_objects/unibind_", collection, "_bindscore_summary.RDS")
+bind_summary <- readRDS(bind_summary_path)
 
 
