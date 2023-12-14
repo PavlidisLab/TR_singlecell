eb03dfd63e96b8c80a79875cf9b6dd28eede8d0b
Alexander-Morin <alexm.14@outlook.com>
2023-12-14 14:15:57 -0800

    rename and update ortho matched targets/tfs

--- R/09d_reverse_coexpr_agg_curated_target_recovery.R
+++ R/09d_reverse_coexpr_agg_curated_target_recovery.R
@@ -1,4 +1,5 @@
-## TODO:
+## Save out a list of the AUC performances of aggregated coexpression's ability
+## to recover curated targets, reversing the lists to prioritize negative coexpr
 ## -----------------------------------------------------------------------------
 
 library(tidyverse)
@@ -43,17 +44,6 @@ rank_tf_mm <- readRDS(rank_tf_mm_path)
 # Curated low throughput targets
 curated <- read.delim(curated_all_path, stringsAsFactors = FALSE)
 
-# Curated TFs with ChIP-seq and all targets for null
-tfs_curated_hg <- intersect(tfs_hg$Symbol, str_to_upper(curated$TF_Symbol))
-tfs_curated_mm <- intersect(tfs_mm$Symbol, str_to_title(curated$TF_Symbol))
-targets_curated_hg <- intersect(pc_hg$Symbol, str_to_upper(curated$Target_Symbol))
-targets_curated_mm <- intersect(pc_mm$Symbol, str_to_title(curated$Target_Symbol))
-
-
-coexpr_auc_hg_path <- "/space/scratch/amorin/R_objects/reverse_coexpr_recover_curated_hg.RDS"
-coexpr_auc_mm_path <- "/space/scratch/amorin/R_objects/reverse_coexpr_recover_curated_mm.RDS"
-
-
 
 # Flip sign of Average_RSR (used as importance score) to make the bottom of
 # the rankings (most consistent negative coexpr) more important
@@ -68,9 +58,55 @@ flip_avgrsr <- function(rank_df) {
 
 
 rank_tf_hg <- lapply(rank_tf_hg, flip_avgrsr)
-
 rank_tf_mm <- lapply(rank_tf_mm, flip_avgrsr)
 
+
+
+# Get ortho-matched symbols of TFs with available data, as well as all targets
+# which are used for null
+# ------------------------------------------------------------------------------
+
+
+# Human
+
+ortho_tf_hg <- pc_ortho %>% 
+  filter(Symbol_mm %in% curated$TF_Symbol | Symbol_hg %in% curated$TF_Symbol) %>% 
+  filter(Symbol_hg %in% names(rank_tf_hg)) %>% 
+  pull(Symbol_hg)
+
+ortho_target_hg <- pc_ortho %>% 
+  filter(Symbol_mm %in% curated$Target_Symbol | Symbol_hg %in% curated$Target_Symbol) %>% 
+  filter(Symbol_hg %in% rownames(rank_tf_hg[[1]])) %>% 
+  pull(Symbol_hg)
+
+tf_hg <- union(
+  intersect(names(rank_tf_hg), str_to_upper(curated$TF_Symbol)),
+  ortho_tf_hg)
+
+target_hg <- union(
+  intersect(rownames(rank_tf_hg[[1]]), str_to_upper(curated$Target_Symbol)),
+  ortho_target_hg)
+
+
+# Mouse
+
+ortho_tf_mm <- pc_ortho %>% 
+  filter(Symbol_mm %in% curated$TF_Symbol | Symbol_hg %in% curated$TF_Symbol) %>% 
+  filter(Symbol_mm %in% names(rank_tf_mm)) %>% 
+  pull(Symbol_mm)
+
+ortho_target_mm <- pc_ortho %>% 
+  filter(Symbol_mm %in% curated$Target_Symbol | Symbol_hg %in% curated$Target_Symbol) %>% 
+  filter(Symbol_mm %in% rownames(rank_tf_mm[[1]])) %>% 
+  pull(Symbol_mm)
+
+tf_mm <- union(
+  intersect(names(rank_tf_mm), str_to_title(curated$TF_Symbol)),
+  ortho_tf_mm)
+
+target_mm <- union(
+  intersect(rownames(rank_tf_mm[[1]]), str_to_title(curated$Target_Symbol)),
+  ortho_target_mm)
 
 
 
@@ -83,12 +119,13 @@ set.seed(5)
 
 # Human 
 
-save_curated_auc_list(path = coexpr_auc_hg_path,
-                      tfs = tfs_curated_hg,
+save_curated_auc_list(path = rev_coexpr_auc_hg_path,
+                      tfs = tf_hg,
                       rank_l = rank_tf_hg,
                       score_col = "Avg_RSR",
                       curated_df = curated,
-                      label_all = targets_curated_hg,
+                      label_all = target_hg,
+                      ortho_df = pc_ortho,
                       pc_df = pc_hg,
                       species = "Human",
                       n_samps = n_samps,
@@ -99,12 +136,13 @@ save_curated_auc_list(path = coexpr_auc_hg_path,
 
 # Mouse 
 
-save_curated_auc_list(path = coexpr_auc_mm_path,
-                      tfs = tfs_curated_mm,
+save_curated_auc_list(path = rev_coexpr_auc_mm_path,
+                      tfs = tf_mm,
                       rank_l = rank_tf_mm,
                       score_col = "Avg_RSR",
                       curated_df = curated,
-                      label_all = targets_curated_mm,
+                      label_all = target_mm,
+                      ortho_df = pc_ortho,
                       pc_df = pc_mm,
                       species = "Mouse",
                       n_samps = 1000,
