9dfb68f3fede53095db1b255f99d80cb9b675ab1
Alexander-Morin <alexm.14@outlook.com>
2023-12-21 14:24:40 -0800

    average cor per dataset; demo scatter

--- R/allcor_heatmap.R
+++ R/allcor_heatmap.R
@@ -1,7 +1,8 @@
 library(pheatmap)
 library(tidyverse)
 
-cor_l <- readRDS("~/Plots/TR_singlecell/All_corplots/Mouse_Ascl1_Dll3.RDS")
+cor_l <- readRDS("~/Plots/TR_singlecell/All_corplots/Human_ASCL1_DLL3.RDS")
+# cor_l <- readRDS("~/Plots/TR_singlecell/All_corplots/Mouse_Ascl1_Dll3.RDS")
 
 
 
@@ -22,6 +23,13 @@ cor_df <- lapply(names(cor_l), function(x) {
   arrange(Cor)
 
 
+cor_group_df <- data.frame(
+  ID = names(cor_l),
+  Mean_cor = unlist(lapply(cor_l, mean))) %>% 
+  arrange(Mean_cor)
+
+
+
 pheatmap(t(cor_df[, "Cor", drop = FALSE]),
          cluster_rows = FALSE,
          cluster_cols = FALSE,
@@ -30,4 +38,47 @@ pheatmap(t(cor_df[, "Cor", drop = FALSE]),
          color = colorRampPalette(c("#0571b0", "white", "#ca0020"))(100),
          breaks = seq(-1, 1, length.out =  100),
          border_color = NA,
-         cellheight = 10)
+         cellheight = 30,
+         # cellwidth = 30,
+         height = 3,
+         filename = file.path(plot_dir, "demo_allcor_heatmap.png"))
+
+
+
+pheatmap(t(cor_group_df[, "Mean_cor", drop = FALSE]),
+         cluster_rows = FALSE,
+         cluster_cols = FALSE,
+         show_rownames = FALSE,
+         show_colnames = FALSE,
+         color = colorRampPalette(c("#0571b0", "white", "#ca0020"))(100),
+         breaks = seq(-1, 1, length.out =  100),
+         border_color = "black",
+         cellheight = 30,
+         # cellwidth = 30,
+         height = 3,
+         filename = file.path(plot_dir, "demo_all_mean_cor_heatmap.png"))
+
+
+
+# demo_id <- "GSE200202"
+# gene1 <- "Ascl1" 
+# gene2 <- "Dll3"
+# dat <- load_dat_list(demo_id)[[1]]
+# mat <- dat$Mat
+# meta <- dat$Meta
+# 
+# # All cell type scatter plots
+# ct_scatter_l <- all_celltype_scatter(mat, meta, gene1, gene2)
+
+
+
+demo_id <- "GarciaAlonso2022Human"
+gene1 <- "ASCL1" 
+gene2 <- "DLL3"
+dat <- load_dat_list(demo_id)[[1]]
+mat <- dat$Mat
+meta <- dat$Meta
+
+# All cell type scatter plots
+ct_scatter_l <- all_celltype_scatter(mat, meta, gene1, gene2)
+
