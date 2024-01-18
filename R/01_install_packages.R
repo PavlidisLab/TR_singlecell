130cffb467c242fb3b83a3f33e00cfea2558706e
Alexander-Morin <alexm.14@outlook.com>
2024-01-18 15:38:27 -0800

    vector package installation

--- R/01_install_packages.R
+++ R/01_install_packages.R
@@ -1,31 +1,47 @@
 ## Collecting packages used. Note that most would have been installed individually.
 ## -----------------------------------------------------------------------------
 
-if (!require("BiocManager")) install.packages('BiocManager')
-
-if (!requireNamespace("remotes")) install.packages("remotes")
 
 
-# BiocManager::install("tanaylab/metacell")
-# remotes::install_github("GfellerLab/SuperCell")
+packages <- c(
+  "parallel",
+  "tidyverse",
+  "BiocManager",
+  "remotes",
+  "Seurat",
+  "WGCNA",
+  "umap",
+  "pheatmap",
+  "RcppTOML",
+  "igraph",
+  "RANN",
+  "WeightedCluster",
+  "corpcor",
+  "weights",
+  "Hmisc",
+  "Matrix",
+  "patchwork",
+  "plyr",
+  "irlba",
+  "DescTools",
+  "googlesheets4",
+  "ROCR",
+  "data.table",
+  "ggrepel"
+)
 
-install.packages("Seurat")
-install.packages("WGCNA")
-install.packages("umap")
-install.packages("RcppTOML")
-install.packages("igraph")
-install.packages("RANN")
-install.packages("WeightedCluster")
-install.packages("corpcor")
-install.packages("weights")
-install.packages("Hmisc")
-install.packages("Matrix")
-install.packages("patchwork")
-install.packages("plyr")
-install.packages("irlba")
-install.packages("DescTools")
-install.packages("lobstr")
-install.packages("googlesheets4")
-install.packages("ROCR")
-install.packages("data.table")
-install.packages("ggrepel")
+
+installed_packages <- packages %in% rownames(installed.packages())
+
+
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+
+
+BiocManager::install("preprocessCore")
+BiocManager::install("GenomicRanges")
+BiocManager::install("limma")
+BiocManager::install("edgeR")
+
