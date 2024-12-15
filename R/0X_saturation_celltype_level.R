99e6736d861d87ae834133ec970c6bae6ae58538
Alexander-Morin <alexm.14@outlook.com>
2024-12-14 16:45:41 -0800

    rm unused; rm max cells from dynamic_steps(); rm iterations per step; init subsample_and_topk_cor(); save/load logic; more plots

--- R/0X_saturation_celltype_level.R
+++ R/0X_saturation_celltype_level.R
@@ -5,51 +5,50 @@ library(tidyverse)
 library(data.table)
 library(parallel)
 library(aggtools)
+library(pheatmap)
 source("R/utils/vector_comparison_functions.R")
 source("R/utils/functions.R")
 source("R/00_config.R")
 
+set.seed(74)
 k <- 200
+min_cells <- 100
+max_steps <- 10
+n_iter <- 10
 
-# Table of assembled scRNA-seq datasets
-sc_meta <- read.delim(sc_meta_path, stringsAsFactors = FALSE)
+id <- "GSE180928"
+ct <- "Neuron"
 
-# IDs for scRNA-seq datasets
-ids_hg <- filter(sc_meta, Species == "Human")$ID
-ids_mm <- filter(sc_meta, Species == "Mouse")$ID
 
-# Protein coding genes, TFs, and L/S ribo genes
-pc_hg <- read.delim(ens_hg_path, stringsAsFactors = FALSE)
-pc_mm <- read.delim(ens_mm_path, stringsAsFactors = FALSE)
-pc_ortho <- read.delim(pc_ortho_path)
-tfs_hg <- read.delim(tfs_hg_path, stringsAsFactors = FALSE)
-tfs_mm <- read.delim(tfs_mm_path, stringsAsFactors = FALSE)
+tfs <- read.delim(tfs_hg_path, stringsAsFactors = FALSE)
 
-# # Measurement matrices used for filtering when a gene was never expressed
-# msr_hg <- readRDS(msr_mat_hg_path)
-# msr_mm <- readRDS(msr_mat_mm_path)
+
+dat <- load_dat_list(id)[[1]]
+
+
 # 
-# # Comeasure matrices tracking count of experiments gene pairs were comeasured
-# comsr_hg <- readRDS(comsr_mat_hg_path)
-# comsr_mm <- readRDS(comsr_mat_mm_path)
-
-# Loading the subset TF and ribosomal aggregate matrices
-# agg_tf_hg <- load_or_generate_agg(path = agg_tf_hg_path, ids = ids_hg, genes = pc_hg$Symbol, sub_genes = tfs_hg$Symbol)
-# agg_tf_mm <- load_or_generate_agg(path = agg_tf_mm_path, ids = ids_mm, genes = pc_mm$Symbol, sub_genes = tfs_mm$Symbol)
-
+# ------------------------------------------------------------------------------
 
 
 # Generate steps of the number of cells to subsample from the total number of cells.
 # Steps are exponentially decided by: (prev_step / ratio) where ratio is assumed
 # to be less than 1.0.
 
-dynamic_steps <- function(max_cells, min_cells, max_steps, ratio = 0.5) {
+# TODO: doc max not included; max_steps means end point could not be reached
+# max when min_cells=100 is 100 / (0.5 ^ 10) == 102400... maybe work backwards?
+# TODO: add to function to remove steps that are less than previous step
+
+dynamic_steps <- function(max_cells,
+                          min_cells = 100,
+                          max_steps = 10, 
+                          ratio = 0.5) {
   
   steps <- c(min_cells)
   
   while (tail(steps, 1) < max_cells) {
-    next_step <- ceiling(tail(steps, 1) / ratio)
     
+    next_step <- ceiling(tail(steps, 1) / ratio)
+
     if (next_step > max_cells || length(steps) >= max_steps) {
       break
     }
@@ -57,95 +56,19 @@ dynamic_steps <- function(max_cells, min_cells, max_steps, ratio = 0.5) {
     steps <- c(steps, next_step)
   }
   
-  steps <- unique(sort(c(steps, max_cells)))
-  
   return(steps)
 }
 
 
 
-iterations_per_step <- function(max_steps, min_iter = 100) {
-  
-  iter <- rep(min_iter, max_steps)
-  iter[max_steps] <- 1  # last step is full count of cells, no need to iter.
-  
-  return(iter)
-  
-}
-
-
-
-
-id <- "GSE180928"
-dat <- load_dat_list(id)[[1]]
-n_cells <- count(dat$Meta, Cell_type, sort = TRUE)
-
-ct <- "Ependymal"
-ct_ids <- filter(dat$Meta, Cell_type == ct)$ID
-max_cells <- filter(n_cells, Cell_type == ct)$n
-min_cells <- 100
-
-
-# TODO: into function that does not collide with aggtools::prepare_celltype_mat
-# (removing genes in advance speeds up)
-
-ct_mat <- t(dat$Mat[, ct_ids])
-keep_genes <- names(which(colSums(ct_mat > 0) >= 20))
-keep_tfs <- intersect(tfs_hg$Symbol, keep_genes)
-ct_mat <- ct_mat[, keep_genes]
-
-
-
-# Full correlation matrix
-cor_mat <- sparse_pcor(ct_mat)
-
-
-n_steps <- 10
-min_iter <- 10
-steps <- dynamic_steps(max_cells = max_cells, min_cells = min_cells, max_steps = n_steps)
-iters <- iterations_per_step(max_steps = n_steps, min_iter = min_iter)
-
-
-# sample_cells <- function(cell_ids, steps, iters) {
-#   
-#   
-# }
-
-
-
-topk_at_iter <- lapply(1:iters[1], function(x) {
-  
-  
-  sample_ids <- sample(ct_ids, steps[4], replace = FALSE)
-  ct_mat_sub <- ct_mat[sample_ids, ]
-  cor_mat_sub <- sparse_pcor(ct_mat_sub)
-  
-  # NA cors to 0 to allow overlap and remove self cor
-  cor_mat_sub[is.na(cor_mat_sub)] <- 0  
-  diag(cor_mat) <- diag(cor_mat_sub) <- 0 
-  
-  # Only consider TFs for overlap
-  topk <- pair_colwise_topk(mat1 = cor_mat[, keep_tfs], 
-                            mat2 = cor_mat_sub[, keep_tfs], 
-                            k = k, 
-                            ncores = ncore)
-  
-  gc(verbose = FALSE)
-  
-  return(topk)
-  
-})
-
-
-topk_at_iter <- bind_rows(topk_at_iter)
-
+# TODO: doc and reduce
 
 generate_summ_df <- function(topk_l) {
   
+  topk_l <- bind_rows(topk_l)
   summ <- sapply(topk_l, summary)
   summ <- t(summ)
   summ <- as.data.frame(summ) %>% rownames_to_column(var = "Symbol")
-  summ <- arrange(summ, Mean)
   summ$Symbol <- factor(summ$Symbol, levels = unique(summ$Symbol))
   
   return(summ)
@@ -153,20 +76,178 @@ generate_summ_df <- function(topk_l) {
 
 
 
+# TODO: Doc
+# TODO: inner loop to function
 
-summ_df <- generate_summ_df(topk_at_iter) 
+subsample_and_topk_cor <- function(ct_mat, 
+                                   steps, 
+                                   n_iters,
+                                   k,
+                                   keep_tfs,
+                                   ncores = 1) {
+  
+  ct_ids <- rownames(ct_mat)
+  
+  # Generate correlation matrix from all cells as reference
+  cor_mat <- sparse_pcor(ct_mat)
+  diag(cor_mat) <- 0  # remove self-cor to prevent inflated topk
+  
+  # Loop through each sample step size, generating cor. matrix n_iter times
+  topk_at_step <- lapply(steps, function(step) {
+    
+    message(paste("Step =", step, Sys.time()))
+    
+    topk_at_iter <- lapply(1:n_iters, function() {
+      
+      sample_ids <- sample(ct_ids, step, replace = FALSE)
+      ct_mat_sub <- ct_mat[sample_ids, ]
+      cor_mat_sub <- sparse_pcor(ct_mat_sub)
+      
+      # NA cors to 0 to allow overlap and remove self cor
+      cor_mat_sub[is.na(cor_mat_sub)] <- 0  
+      diag(cor_mat_sub) <- 0 
+      
+      # Only consider TFs for overlap
+      topk <- pair_colwise_topk(mat1 = cor_mat[, keep_tfs], 
+                                mat2 = cor_mat_sub[, keep_tfs], 
+                                k = k, 
+                                ncores = ncores)
+      
+      gc(verbose = FALSE)
+      
+      return(topk)
+      
+    })
+    
+    # Summary of topk for each TF across iterations
+    generate_summ_df(topk_at_iter)
+    
+  })
+  names(topk_at_step) <- paste0("step_", steps)
+  
+  return(topk_at_step)
+}
 
 
-ggplot(summ_df, aes(x = Symbol, y = Mean)) +
-  geom_crossbar(aes(x = Symbol, ymin = `1st Qu.`, ymax = `3rd Qu.`)) +
-  geom_point(aes(x = Symbol, y = Mean), shape = 21, colour = "firebrick") +
-  theme_classic() +
-  theme(text = element_text(size = 20),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+
+#
+# ------------------------------------------------------------------------------
+
+
+ct_ids <- filter(dat$Meta, Cell_type == ct)$ID
+ct_mat <- dat$Mat[, ct_ids]
+
+keep_genes <- names(which(rowSums(ct_mat > 0) >= 20))
+keep_tfs <- intersect(tfs$Symbol, keep_genes)
+ct_mat <- t(ct_mat[keep_genes, ])
+
+max_cells <- nrow(ct_mat)
+
+
+steps <- dynamic_steps(max_cells = max_cells, min_cells = min_cells)
 
 
 
+file <- file.path("/space/scratch/amorin/R_objects/TRsc", 
+                  paste("subsample_cor", id, ct, n_iter, "iters.RDS", sep = "_"))
+
+
+
+if (!file.exists(file)) {
+  
+  subsample_l <- subsample_and_topk_cor(
+    ct_mat = ct_mat,
+    steps = steps,
+    n_iters = n_iters,
+    k = k,
+    keep_tfs = keep_tfs,
+    ncores = ncore)
+  
+  saveRDS(subsample_l, file)
+  
+} else {
+  
+  subsample_l <- readRDS(file)
+  
+}
+
+
+
+stop()
+
+topk_barplot <- function(summ_df) {
+  
+  ggplot(summ_df, aes(x = Symbol, y = Mean)) +
+    geom_crossbar(aes(x = Symbol, ymin = `1st Qu.`, ymax = `3rd Qu.`)) +
+    geom_point(aes(x = Symbol, y = Mean), shape = 21, colour = "firebrick") +
+    ylab("Top200") +
+    theme_classic() +
+    theme(text = element_text(size = 20),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank())
+  
+}
+
+
+
+
+pl1 <- lapply(subsample_l, topk_barplot)
+
+
+# TODO: mean vs median
+
+mean_df <- lapply(1:length(steps), function(x) {
+  
+  data.frame(Group = rep(paste0("Step=", steps[x]), length(keep_tfs)),
+             Topk = unlist(subsample_l[[x]]$Mean)) %>% 
+    mutate(Group = factor(Group, levels = unique(Group)))
+  
+}) %>% 
+  bind_rows()
+
+
+
+ggplot(mean_df, aes(x = Topk, colour = Group)) +
+  geom_density()
+
+
+
+mean_df <- lapply(subsample_l, function(x) {
+  
+  x %>% 
+    arrange(match(Symbol, keep_tfs)) %>% 
+    pull(Mean)
+  
+}) %>% 
+  bind_cols() %>% 
+  as.matrix()
+
+rownames(mean_df) <- keep_tfs
+colnames(mean_df) <- paste0("Step=", steps)
+mean_df <- mean_df[order(mean_df[, length(steps)]), ]
+
+
+# cols <- c('#f7f4f9','#e7e1ef','#d4b9da','#c994c7','#df65b0','#e7298a','#ce1256','#980043','#67001f')
+cols <- viridis::inferno(10)
+
+pheatmap(mean_df,
+         cluster_rows = FALSE,
+         cluster_cols = FALSE,
+         show_rownames = FALSE,
+         color = cols)
+
+
+avg_expr <- colMeans(log2(ct_mat[, keep_tfs] + 1))
+
+
+summary_df <- data.frame(Symbol = keep_tfs, 
+                         Avg_expr = avg_expr,
+                         mean_df[keep_tfs, ])
+
+
+
+cor(select_if(summary_df, is.numeric), method = "spearman")
+# plot(summary_df$Avg_expr, summary_df$Step.25600)
 
 
 ##
@@ -184,3 +265,36 @@ ggplot(summ_df, aes(x = Symbol, y = Mean)) +
 #                           mat2 = cor_mat_sub[, keep_tfs], 
 #                           k = k, 
 #                           ncores = ncore)
+
+
+
+# topk_at_step <- lapply(steps, function(step) {
+#   
+#   message(paste("Step =", step, Sys.time()))
+#   
+#   topk_l <- lapply(1:10, function(x) {
+#     
+#     sample_ids <- sample(ct_ids, step, replace = FALSE)
+#     ct_mat_sub <- ct_mat[sample_ids, ]
+#     cor_mat_sub <- sparse_pcor(ct_mat_sub)
+#     
+#     # NA cors to 0 to allow overlap and remove self cor
+#     cor_mat_sub[is.na(cor_mat_sub)] <- 0  
+#     diag(cor_mat) <- diag(cor_mat_sub) <- 0 
+#     
+#     # Only consider TFs for overlap
+#     topk <- pair_colwise_topk(mat1 = cor_mat[, keep_tfs], 
+#                               mat2 = cor_mat_sub[, keep_tfs], 
+#                               k = k, 
+#                               ncores = ncore)
+#     
+#     gc(verbose = FALSE)
+#     
+#     return(topk)
+#     
+#   })
+#   
+#   generate_summ_df(topk_l)
+#   
+# })
+# names(topk_at_step) <- paste0("step_", steps)
