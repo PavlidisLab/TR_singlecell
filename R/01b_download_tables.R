c0ed39e4eb5cc8661130af80d042c37baaf46ef1
Alexander-Morin <alexm.14@outlook.com>
2025-01-27 23:05:40 -0800

    process_and_save_pc_tables()

--- R/01b_download_tables.R
+++ R/01b_download_tables.R
@@ -152,59 +152,51 @@ download_ensembl_pcoding <- function(outfile,
 
 
 
+# Ensuring Refseq and Ensembl share the same symbols. Refseq used for most 
+# analyses, Ensembl to map Ensembl IDs to symbols for datasets with IDs only.
+
+process_and_save_pc_tables <- function(ref_path, ens_path) {
+  
+  # Load tables
+  ref_table <- read.delim(ref_path, stringsAsFactors = FALSE)
+  ens_table <- read.delim(ens_path, stringsAsFactors = FALSE)
+  
+  # Early exit if Refseq_ID already in ensembl - job is already done!
+  if ("Refseq_ID" %in% colnames(ens_table)) {
+    return(message("Protein coding tables already match!"))
+  }
+  
+  # Keep only X copies of duplicated genes (for human; mouse don't have these dupls)
+  dupl_genes <- ref_table %>% group_by(Symbol) %>% filter(n() > 1)
+  ref_table <- filter(ref_table, !(Symbol %in% dupl_genes$Symbol & Chromosome == "Y"))
+  
+  # Reduce Ensembl to Refseq select, keeping only a single entry per symbol, and
+  # only the X copy of duplicated counts. As using gene-summarized counts, the 
+  # exact transcript is unimportant
+  ens_table <- ens_table %>%
+    filter(Symbol %in% ref_table$Symbol & Symbol != "") %>%
+    right_join(ref_table[, c("Symbol", "Refseq_ID")], by = "Symbol") %>%
+    filter(!(Symbol %in% dupl_genes$Symbol & Chromosome == "Y")) %>%
+    distinct(Symbol, .keep_all = TRUE) %>%
+    arrange(match(Symbol, ref_table$Symbol))
+  
+  write.table(ref_table, sep = "\t", quote = FALSE, file = ref_path)
+  write.table(ens_table, sep = "\t", quote = FALSE, file = ens_path)
+  
+  return(TRUE)
+  
+}
+
+
+
 download_refseq(outfile = ref_hg_path, species = "Human")
 download_refseq(outfile = ref_mm_path, species = "Mouse")
-
 
 download_ensembl_pcoding(outfile = ens_hg_path, species = "Human")
 download_ensembl_pcoding(outfile = ens_mm_path, species = "Mouse")
 
-
-# Ensuring Refseq and Ensembl share the same symbols. Refseq used for most 
-# analyses, Ensembl to map Ensembl IDs to symbols for datasets with IDs only
-
-
-ref_hg <- read.delim(ref_hg_path, stringsAsFactors = FALSE)
-ref_mm <- read.delim(ref_mm_path, stringsAsFactors = FALSE)
-ens_hg <- read.delim(ens_hg_path, stringsAsFactors = FALSE)
-ens_mm <- read.delim(ens_mm_path, stringsAsFactors = FALSE)
-
-
-# Keep only X copies of duplicated genes in human
-
-dupl_genes <- ref_hg %>% group_by(Symbol) %>% filter(n() > 1)
-
-ref_hg <- filter(ref_hg, !(Symbol %in% dupl_genes$Symbol & Chromosome == "Y"))
-
-  
-# Reduce Ensembl to Refseq select, keeping only a single entry per symbol, and
-# only the X copy of duplicated counts. As using gene-summarized counts, the 
-# exact transcript is unimportant
-
-
-ens_hg <- ens_hg %>%
-  filter(Symbol %in% ref_hg$Symbol & Symbol != "") %>%
-  right_join(ref_hg[, c("Symbol", "Refseq_ID")], by = "Symbol") %>%
-  filter(!(Symbol %in% dupl_genes$Symbol & Chromosome == "Y")) %>%
-  distinct(Symbol, .keep_all = TRUE) %>%
-  arrange(match(Symbol, ref_hg$Symbol))
-
-
-ens_mm <- ens_mm %>% 
-  filter(Symbol %in% ref_mm$Symbol & Symbol != "") %>% 
-  right_join(ref_mm[, c("Symbol", "Refseq_ID")], by = "Symbol") %>% 
-  distinct(Symbol, .keep_all = TRUE) %>%
-  arrange(match(Symbol, ref_mm$Symbol))
-
-
-stopifnot(identical(ens_hg$Symbol, ref_hg$Symbol))
-stopifnot(identical(ens_mm$Symbol, ref_mm$Symbol))
-
-
-write.table(ref_hg, sep = "\t", quote = FALSE, file = ref_hg_path)
-write.table(ref_mm, sep = "\t", quote = FALSE, file = ref_mm_path)
-write.table(ens_hg, sep = "\t", quote = FALSE, file = ens_hg_path)
-write.table(ens_mm, sep = "\t", quote = FALSE, file = ens_mm_path)
+process_and_save_pc_tables(ref_hg_path, ens_hg_path)
+process_and_save_pc_tables(ref_mm_path, ens_mm_path)
 
 
 
