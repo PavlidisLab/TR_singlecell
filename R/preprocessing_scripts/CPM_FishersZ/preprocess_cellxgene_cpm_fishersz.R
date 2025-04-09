16df49edaa145d75a991852c2ef50d8853b772bd
Alexander-Morin <alexm.14@outlook.com>
2025-04-09 14:26:47 -0700

    missing library call; cor/agg_method to vars; add force_resave var; load multi cxg; filter is_primary_data; add aggtools impl and frwite_mat()

--- R/preprocessing_scripts/CPM_FishersZ/preprocess_cellxgene_cpm_fishersz.R
+++ R/preprocessing_scripts/CPM_FishersZ/preprocess_cellxgene_cpm_fishersz.R
@@ -3,6 +3,7 @@
 ## the aggregate correlation matrix and a matrix tracking NAs
 ## -----------------------------------------------------------------------------
 
+library(aggtools)
 library(tidyverse)
 library(data.table)
 library(Seurat)
@@ -12,16 +13,20 @@ source("R/utils/plot_functions.R")
 
 args <- commandArgs(trailingOnly = TRUE)
 id <- args[1]
-species <- args[2]  
+species <- args[2]
+cor_method <- "pearson"
+agg_method <- "FZ"
+force_resave <- FALSE
 
 dat_dir <- file.path(sc_dir, id)
-dat_path <- file.path(dat_dir, paste0(id, "_cellxgene_seurat.RDS"))
+dat_path <- list.files(dat_dir, pattern = "^.*_seurat.*.RDS", full.names = TRUE)
 out_dir <- file.path(amat_dir, id)
 processed_path <- file.path(out_dir, paste0(id, "_clean_mat_and_meta_CPM.RDS"))
-aggmat_path <- file.path(out_dir, paste0(id, "_FishersZ_CPM.tsv"))
+amat_path <- file.path(out_dir, paste0(id, "_FZ_CPM.tsv"))
 namat_path <- file.path(out_dir, paste0(id, "_NA_mat.tsv"))
 
 
+# Protein coding gene table (all CxG are ENSEMBL)
 pc <- if (str_to_lower(species) %in% c("human", "hg")) {
   read.delim(ens_hg_path, stringsAsFactors = FALSE)
 } else if (str_to_lower(species) %in% c("mouse", "mm")) {
@@ -32,12 +37,13 @@ pc <- if (str_to_lower(species) %in% c("human", "hg")) {
 
 
 
-if (!file.exists(processed_path)) {
+if (!file.exists(processed_path) || force_resave) {
   
-  dat <- readRDS(dat_path)
+  # Load Seurat object -- if multiple, merge into one
+  dat <- lapply(dat_path, readRDS)
+  dat <- reduce(dat, merge)
   
   # Extract count matrix: default counts slot, but use data slot if counts empty
-  
   mat <- GetAssayData(dat, slot = "counts")
   
   if (length(mat) == 0 || all(rowSums(mat) == 0)) {
@@ -47,18 +53,19 @@ if (!file.exists(processed_path)) {
   stopifnot(typeof(mat@i) == "integer")
   
   
-  # Ready metadata
-  
+  # Ready metadata and ensure cells are not duplicated across collections 
   change_colnames <- c(Cell_type = "cell_type", Old_ID = "ID")
   
   meta <- dat[[]] %>% 
     dplyr::rename(any_of(change_colnames)) %>% 
     rownames_to_column(var = "ID") %>% 
-    add_count_info(mat = mat)
+    filter(is_primary_data)
+  
+  mat <- mat[, meta$ID]
+  meta <- add_count_info(meta, mat)
   
   
   # QC plots
-  
   p1 <- all_hist(meta)
   p2 <- qc_scatter(meta)
   
@@ -68,8 +75,8 @@ if (!file.exists(processed_path)) {
   ggsave(p2, device = "png", dpi = 300, height = 8, width = 8,
          filename = file.path(out_dir, paste0(id, "_QC_scatter.png")))
   
-  # Remove cells failing QC, keep only protein coding genes, and normalize
   
+  # Remove cells failing QC, keep only protein coding genes, and normalize
   mat <- rm_low_qc_cells(mat, meta) %>%
     ensembl_to_symbol(ensembl_df = pc) %>% 
     get_pcoding_only(pcoding_df = pc) %>% 
@@ -88,43 +95,30 @@ if (!file.exists(processed_path)) {
   rm(dat)
   gc()
   
-} else {
+} 
+
+
+
+if (!file.exists(amat_path)) {
   
+  # Load processed data
   dat <- readRDS(processed_path)
   meta <- dat$Meta
   mat <- dat$Mat
+  stopifnot(identical(colnames(mat), meta$ID))
   
-}
-
-
-stopifnot(identical(colnames(mat), meta$ID))
-
-
-
-if (!file.exists(aggmat_path)) {
-  
-  agg <- fishersZ_aggregate(mat, meta)
-  
-  # Write as data.frames (preserve rownames) with data.table fwrite (fast)
-  
-  fwrite(
-    data.frame(agg$Agg_mat, check.names = FALSE),
-    sep = "\t",
-    row.names = TRUE,
-    quote = FALSE,
-    verbose = FALSE,
-    showProgress = FALSE,
-    file = aggmat_path
+  # Run coexpression aggregate
+  agg_l <- aggr_coexpr_single_dataset(
+    mat = mat, 
+    meta = meta,
+    pc_df = pc,
+    cor_method = cor_method,
+    agg_method = agg_method
   )
+
   
+  # Save output matrices as .tsv
+  fwrite_mat(agg_l$Agg_mat, amat_path)
+  fwrite_mat(agg_l$NA_mat, namat_path)
   
-  fwrite(
-    data.frame(agg$NA_mat, check.names = FALSE),
-    sep = "\t",
-    row.names = TRUE,
-    quote = FALSE,
-    verbose = FALSE,
-    showProgress = FALSE,
-    file = namat_path
-  )
 }
